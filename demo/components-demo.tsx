e14e9a1a6fdb5771ec2e10967440a3e7662625d7
Kieran Williams <kieranw@wix.com>
2017-07-09 16:45:35 +0300

    Added "demo"

--- demo/components-demo.tsx
+++ demo/components-demo.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { Link } from '../src';
+import { Image } from '../src';
+import { sampleImage } from '../test/fixtures/sample-image';
 const style = require('./style.css');
 import {Provider} from 'mobx-react';
 
@@ -9,6 +11,7 @@ export class ComponentsDemo extends React.Component<{}, {}>{
             <Provider>
                 <div>
                     <Link className={style.test} href="">My link</Link>
+                    <Image src={sampleImage} />
                 </div>
             </Provider>
         );
