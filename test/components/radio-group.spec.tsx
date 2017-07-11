35bba53b6d6d249a69a6c5f86691db2ba150205d
WIXPRESS\danielstr <danielstr@wix.com>
2017-07-11 11:11:05 +0300

    adding component test

--- test/components/radio-group.spec.tsx
+++ test/components/radio-group.spec.tsx
@@ -1,22 +1,41 @@
 import React = require('react');
-import { expect, ClientRenderer } from 'test-drive-react';
+import {expect, ClientRenderer, simulate} from 'test-drive-react';
 import { RadioButton } from '../../src'
+import { RadioGroupDemo } from '../../demo/radio-group-demo';
 
+const radioGroup = 'RADIO_GROUP';
 const radioButton = 'RADIO_BUTTON';
 
-describe('<RadioButton />', function () {
+describe('<RadioGroup />', function () {
     const clientRenderer = new ClientRenderer();
 
     afterEach(() => {
         clientRenderer.cleanup();
     });
 
-    it('renders a radio button to the screen', function () {
-        const { select, waitForDom } = clientRenderer.render(<RadioButton value="Shiva"/>);
+    describe('The radio group user', function () {
+        it('clicks on a button and it is selected', async function () {
+            const {select, waitForDom} = clientRenderer.render(<RadioGroupDemo/>);
 
-        return waitForDom(() => {
-            expect(select(radioButton)).to.be.present().and.to.have.attr('type', 'radio');
-            expect(select(radioButton)).to.have.attr('value', 'Shiva');
+            await waitForDom(() => { expect(select(radioGroup, radioButton + '_0')).to.be.present() });
+            simulate.click(select(radioGroup, radioButton + '_0'));
+            return await waitForDom(() => {
+                expect(select('RADIO_GROUP_DEMO_VALUE')).to.have.text('This way!');
+            });
         });
     });
+
+    describe('<RadioButton />', function () {
+        it('renders a radio button to the screen', function () {
+            const { select, waitForDom } = clientRenderer.render(<RadioButton value="Shiva"/>);
+
+            return waitForDom(() => {
+                expect(select(radioButton)).to.be.present().and.to.have.attr('type', 'radio');
+                expect(select(radioButton)).to.have.attr('value', 'Shiva');
+            });
+        });
+    });
+
+
 });
+
