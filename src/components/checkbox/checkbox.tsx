6d84718cd25fc07908d8c89ef36b453e43573795
Jonathan Marton <marton@wix.com>
2017-07-12 14:26:54 +0300

    refactor click handler outside the component

--- src/components/checkbox/checkbox.tsx
+++ src/components/checkbox/checkbox.tsx
@@ -9,38 +9,33 @@ interface CheckBoxProps {
 
 }
 
-export const CheckBox: React.SFC<CheckBoxProps> = (props) => {
+export const CheckBox: React.SFC<CheckBoxProps> = (props) => (
+    <div data-automation-id="CHECKBOX_ROOT"
+         onClick={(event) => executeClickHandler(props.onChange!, !props.value)}>
+        <img data-automation-id="CHECKBOX_BOX"
+             src={props.boxIcon}
+             style={{width: '1em', height: '1em'}}/>
+        <img data-automation-id="CHECKBOX_TICKMARK"
+             src={props.tickIcon}
+             style={{
+                 width: '1em',
+                 height: '1em',
+                 visibility: props.value ? 'visible' : 'hidden',
+                 position: 'relative',
+                 marginLeft: '-1em'
+             }}/>
+        <span data-automation-id="CHECKBOX_LABEL">{props.text || ''}</span>
+    </div>
+);
 
-    const handleClick = (event: React.SyntheticEvent<HTMLElement>): void => {
-        props.onChange && props.onChange(!props.value)
-    };
-
-    return (
-        <div data-automation-id="CHECKBOX_ROOT"
-             onClick={(event) => handleClick(event)}>
-            <img data-automation-id="CHECKBOX_BOX"
-                 src={props.boxIcon}
-                 style={{width: '1em', height: '1em'}}/>
-            <img data-automation-id="CHECKBOX_TICKMARK"
-                 src={props.tickIcon}
-                 style={{
-                     width: '1em',
-                     height: '1em',
-                     visibility: props.value ? 'visible' : 'hidden',
-                     position: 'relative',
-                     marginLeft: '-1em'
-                 }}/>
-            <span data-automation-id="CHECKBOX_LABEL">{props.text || ''}</span>
-        </div>
-    )
-};
-
+function executeClickHandler(handler: (value: boolean) => any, value: boolean) {
+    handler && handler(value);
+}
 
 CheckBox.defaultProps = {
     text: '',
     value: false,
     boxIcon: '/src/components/checkbox/uncheckedCheckbox.svg',
     tickIcon: '/src/components/checkbox/tickMark.svg',
-    onChange: ((_: boolean) => {
-    })
+    onChange: ((_: boolean) => {})
 };
