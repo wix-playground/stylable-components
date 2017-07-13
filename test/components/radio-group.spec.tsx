d46b93b82c5c3691ce05912eb20b43c6718a918a
WIXPRESS\danielstr <danielstr@wix.com>
2017-07-13 14:33:12 +0300

    added test for multiple radio groups

--- test/components/radio-group.spec.tsx
+++ test/components/radio-group.spec.tsx
@@ -100,6 +100,36 @@ describe('<RadioGroup />', function () {
         });
     });
 
+    it('does not affect buttons in a different radio group', async function () {
+        const {select, waitForDom} = clientRenderer.render(
+            <div>
+                <div data-automation-id="DIV_0">
+                    <RadioGroup onChange={emptyFunction}>
+                        <RadioButton value="Siren"/>
+                        <RadioButton value="Cerberus"/>
+                    </RadioGroup>
+                </div>
+                <div data-automation-id="DIV_1">
+                    <RadioGroup onChange={emptyFunction}>
+                        <RadioButton value="Alexander"/>
+                        <RadioButton value="Odin"/>
+                    </RadioGroup>
+                </div>
+            </div>
+        );
+
+        await waitForDom(() => { expect(select('DIV_0', radioGroup, radioButton + '_0')).to.be.present() });
+        const button0 = select('DIV_0', radioGroup, radioButton + '_0') as HTMLInputElement;
+        button0.click();
+        await waitForDom(() => { expect(button0.checked).to.be.true; });
+        const button1 = select('DIV_1', radioGroup, radioButton + '_1') as HTMLInputElement;
+        button1.click();
+        return waitForDom(() => {
+            expect(button0.checked).to.be.true;
+            expect(button1.checked).to.be.true;
+        });
+    });
+
     describe('<RadioButton />', function () {
         it('renders a radio button to the screen', function () {
             const { select, waitForDom } = clientRenderer.render(<RadioButton value="Shiva" automationId={radioButton + '_0'} name=""/>);
@@ -121,7 +151,7 @@ describe('<RadioGroup />', function () {
 
         it('calls the onClick function when clicked', async function () {
             const onClick = sinon.spy();
-            const { select, waitForDom } = clientRenderer.render(<RadioButton value="Shiva" onClick={onClick} automationId={radioButton + '_0'} name=""/>);
+            const { select, waitForDom } = clientRenderer.render(<RadioButton value="Tonberry" onClick={onClick} automationId={radioButton + '_0'} name=""/>);
 
             await waitForDom(() => { expect(select(radioButton + '_0')).to.be.present() });
             simulate.click(select(radioButton + '_0'));
