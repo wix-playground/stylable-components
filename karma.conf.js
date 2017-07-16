7e7e09a6d64601eebef219962ce5faf9861cd04c
Avi Vahl <avi.vahl@wix.com>
2017-07-16 20:46:00 +0300

    Revert forced SauceLabs in branch left commented out line to force test easily

--- karma.conf.js
+++ karma.conf.js
@@ -135,8 +135,8 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        // browsers: process.env.TRAVIS_BRANCH === 'master' && process.env.TRAVIS_PULL_REQUEST === 'false' ?
-        browsers: true ?
+        browsers: process.env.TRAVIS_BRANCH === 'master' && process.env.TRAVIS_PULL_REQUEST === 'false' ?
+        // browsers: true ?
             Object.keys(sauceLabsLaunchers) :
             ['Chrome'],
 
