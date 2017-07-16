189e6f64cfe02ef2202bd619f470a097b89a0122
Avi Vahl <avi.vahl@wix.com>
2017-07-16 18:34:17 +0300

    Don't run for pull requests as well (where branch name is master)

--- karma.conf.js
+++ karma.conf.js
@@ -128,7 +128,9 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: process.env.TRAVIS_BRANCH === 'master' ? Object.keys(sauceLabsLaunchers) : ['Chrome'],
+        browsers: process.env.TRAVIS_BRANCH === 'master' && !process.env.TRAVIS_PULL_REQUEST ?
+            Object.keys(sauceLabsLaunchers) :
+            ['Chrome'],
 
 
         // Continuous Integration mode
