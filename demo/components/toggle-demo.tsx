bec6f17936c86e3c026b06ef936d9dc025ef3192
Evgen Filatov <evgen.filatov@gmail.com>
2017-07-21 17:26:14 +0300

    added toggle code

--- demo/components/toggle-demo.tsx
+++ demo/components/toggle-demo.tsx
@@ -2,9 +2,49 @@ import * as React from 'react';
 import {Toogle} from '../../src';
 
 export class ToogleDemo extends React.Component<{}, {}> {
+    state = {
+        checked1: false,
+        checked2: false
+    }
     render () {
-        return <div>
-            <Toogle/>
-        </div>
+        return <table>
+            <tbody>
+                <tr>
+                    <th>
+                        Controled
+                        <Toogle
+                            label='with icons'
+                            checked={this.state.checked1}
+                            onChange={checked1 => this.setState({checked1})}
+                        />
+                    </th>
+                    <th>
+                        No icons
+                        <Toogle
+                            label='without icons'
+                            displayIcons={false}
+                            checked={this.state.checked2}
+                            onChange={checked2 => this.setState({checked2})}
+                        />
+                    </th>
+                    <th>
+                        Off
+                        <Toogle/>
+                    </th>
+                    <th>
+                        On
+                        <Toogle checked/>
+                    </th>
+                    <th>
+                        Disabled off
+                        <Toogle disabled/>
+                    </th>
+                    <th>
+                        Disabled on
+                        <Toogle disabled checked/>
+                    </th>
+                </tr>
+            </tbody>
+        </table>
     }
 }
--- src/components/toggle/toggle.tsx
+++ src/components/toggle/toggle.tsx
@@ -1,11 +1,55 @@
 import * as React from 'react';
-import {SBStateless} from 'stylable-react-component'
+import {SBStateless} from 'stylable-react-component';
+import style from './toggle.st.css';
 
-export Props {
+export interface Props {
+	checked?: boolean,
+	disabled?: boolean,
+	onChange?: (selected: boolean) => void,
+	checkedIcon?: JSX.Element,
+	uncheckedIcon?: JSX.Element,
+	label?: string,
+	displayIcons?: boolean
 }
+
+const defaultCheckedIcon = <svg width='10' height='10' viewBox='0 0 41 32'><path fill='currentColor' d='M0.169 17.815c0.169 1.098 0.76 2.111 1.689 2.871l14.269 10.385c1.942 1.435 4.644 1.013 6.079-0.844l18.069-23.303c1.435-1.858 1.098-4.559-0.844-5.995s-4.644-1.098-6.164 0.844l-15.367 19.842-10.723-7.852c-1.942-1.435-4.644-1.013-6.164 0.844-0.76 0.929-1.013 2.111-0.844 3.208z'></path></svg>
+const defaultUncheckedIcon = <svg width='10' height='10'viewBox='0 0 143 32'><path fill='currentColor' d='M0 0h142.545v32h-142.545v-32z'/></svg>
 
 function Toogle(props: Props) {
-	return <div></div>
+	const {
+		checked = false,
+		disabled = false,
+		displayIcons = true,
+		checkedIcon = defaultCheckedIcon,
+		uncheckedIcon = defaultUncheckedIcon,
+		label,
+		onChange
+	} = props;
+	const toggle = () => {
+		if (onChange && !disabled) {
+			onChange(!checked)
+		}
+	}
+
+	return <div
+		onClick={toggle}
+		cssStates={{checked, disabled}}
+	>
+		{!disabled &&
+			<input
+				className='input'
+				type='checkbox'
+				aria-label={label}
+				checked={checked}
+			/>
+		}
+		<div className='background'>
+			<div
+				className='switch'
+				children={displayIcons ? (checked ? checkedIcon : uncheckedIcon) : null}
+			/>
+		</div>
+	</div>
 }
 
-export default Toogle
+export default SBStateless(Toogle, style)
