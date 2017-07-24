b88458ec129243a7759e6d0d74310e1675fb2b55
Evgen Filatov <evgen.filatov@gmail.com>
2017-07-24 18:07:34 +0300

    all toggle tests are green

--- src/components/toggle/toggle.tsx
+++ src/components/toggle/toggle.tsx
@@ -6,18 +6,18 @@ export interface Props {
 	checked?: boolean,
 	disabled?: boolean,
 	onChange?: (selected: boolean) => void,
-	checkedIcon?: JSX.Element,
-	uncheckedIcon?: JSX.Element,
+	iconUnchecked?: JSX.Element,
+	iconChecked?: JSX.Element,
 	label?: string,
-	displayIcons?: boolean
+	displayIcon?: boolean
 }
 
-const defaultCheckedIcon = (
+const defaultIconChecked = (
 	<svg width='10' height='10' viewBox='0 0 41 32' data-automation-id='TOGGLE_CHECKED_ICON'>
 		<path fill='currentColor' d='M0.169 17.815c0.169 1.098 0.76 2.111 1.689 2.871l14.269 10.385c1.942 1.435 4.644 1.013 6.079-0.844l18.069-23.303c1.435-1.858 1.098-4.559-0.844-5.995s-4.644-1.098-6.164 0.844l-15.367 19.842-10.723-7.852c-1.942-1.435-4.644-1.013-6.164 0.844-0.76 0.929-1.013 2.111-0.844 3.208z'/>
 	</svg>
 );
-const defaultUncheckedIcon = (
+const defaultIconUnchecked = (
 	<svg width='10' height='10'viewBox='0 0 143 32' data-automation-id='TOGGLE_UNCHECKED_ICON'>
 		<path fill='currentColor' d='M0 0h142.545v32h-142.545v-32z'/>
 	</svg>
@@ -27,9 +27,9 @@ function Toogle(props: Props) {
 	const {
 		checked = false,
 		disabled = false,
-		displayIcons = true,
-		checkedIcon = defaultCheckedIcon,
-		uncheckedIcon = defaultUncheckedIcon,
+		displayIcon = true,
+		iconChecked = defaultIconChecked,
+		iconUnchecked = defaultIconUnchecked,
 		label,
 		onChange
 	} = props;
@@ -56,7 +56,7 @@ function Toogle(props: Props) {
 		<div className='background'>
 			<div
 				className='switch'
-				children={displayIcons ? (checked ? checkedIcon : uncheckedIcon) : null}
+				children={displayIcon ? (checked ? iconChecked : iconUnchecked) : null}
 			/>
 		</div>
 	</div>
--- test/components/toggle.spec.tsx
+++ test/components/toggle.spec.tsx
@@ -8,25 +8,38 @@ describe('<Toggle/>', () => {
     afterEach(() => clientRenderer.cleanup());
 
     describe('render without options', () => {
-        const {select, waitForDom} = clientRenderer.render(<Toogle/>)
+        let onChange: any
+        let renderer: any
+        beforeEach(() => {
+            onChange = sinon.spy();
+            renderer = clientRenderer.render(<Toogle onChange={onChange}/>)
+        })
         it('should have input', () => {
-            return waitForDom(() => expect(select('TOGGLE_INPUT')).to.not.null);
+            expect(renderer.select('TOGGLE_INPUT')).to.not.null
         })
         it('should render default unchecked icon', () => {
-            return waitForDom(() => expect(select('TOGGLE_UNCHECKED_ICON')).to.not.null);
+            expect(renderer.select('TOGGLE_UNCHECKED_ICON')).to.not.null
         })
         it('should not render default checked icon', () => {
-            return waitForDom(() => expect(select('TOGGLE_CHECKED_ICON')).to.null);
+            expect(renderer.select('TOGGLE_CHECKED_ICON')).to.be.null
         })
     })
 
     describe('render with checked={true}', () => {
-        const {select, waitForDom} = clientRenderer.render(<Toogle checked/>)
-        it('should not render default unchecked icon', () => {
-            return waitForDom(() => expect(select('TOGGLE_UNCHECKED_ICON')).to.null);
+        let onChange: any
+        let renderer: any
+        beforeEach(() => {
+            onChange = sinon.spy();
+            renderer = clientRenderer.render(<Toogle onChange={onChange} checked/>)
         })
-        it('should not render default checked icon', () => {
-            return waitForDom(() => expect(select('TOGGLE_CHECKED_ICON')).to.not.null);
+        it('should have input', () => {
+            expect(renderer.select('TOGGLE_INPUT')).to.not.null
+        })
+        it('should not render default unchecked icon', () => {
+            expect(renderer.select('TOGGLE_UNCHECKED_ICON')).to.be.null
+        })
+        it('should render default checked icon', () => {
+            expect(renderer.select('TOGGLE_CHECKED_ICON')).to.not.null
         })
     })
 
@@ -44,15 +57,68 @@ describe('<Toggle/>', () => {
             expect(onChange).to.have.been.calledWithExactly(true);
         })
 
-        it('pressing space on focus should trigger onChange', () => {
+        it('pressing space on focused input should trigger onChange', () => {
             const input = renderer.select('TOGGLE_INPUT');
             simulate.focus(input)
-            simulate.keyPress(input, {keyCode: 13})
-            simulate.keyPress(input, {keyCode: 32})
-            simulate.keyUp(input, {keyCode: 32})
-            simulate.keyUp(input, {keyCode: 13})
+            simulate.click(input)
             expect(onChange).to.have.been.calledOnce;
             expect(onChange).to.have.been.calledWithExactly(true);
+        })
+    })
+
+    describe('render with disabled={true}', () => {
+        let onChange: any
+        let renderer: any
+        beforeEach(() => {
+            onChange = sinon.spy();
+            renderer = clientRenderer.render(<Toogle onChange={onChange} disabled/>)
+        })
+        it('should not have input underhood', () => {
+            expect(renderer.select('TOGGLE_INPUT')).to.be.null;
+        })
+
+        it('click should not trigger onChange', () => {
+            simulate.click(renderer.select('TOGGLE'))
+            expect(onChange).to.not.have.been.called;
+        })
+    })
+
+    describe('render with custom icons', () => {
+        it('unchecked should have custom iconUnchecked', () => {
+            const onChange = sinon.spy();
+            const renderer = clientRenderer.render(
+                <Toogle
+                    onChange={onChange}
+                    iconChecked={<div data-automation-id='TEST_ICON_CHECKED'/>}
+                    iconUnchecked={<div data-automation-id='TEST_ICON_UNCHECKED'/>}
+                />
+            )
+            expect(renderer.select('TEST_ICON_UNCHECKED')).to.not.null;
+            expect(renderer.select('TEST_ICON_CHECKED')).to.be.null;
+        })
+
+        it('unchecked should have custom iconChecked', () => {
+            const onChange = sinon.spy();
+            const renderer = clientRenderer.render(
+                <Toogle
+                    checked
+                    onChange={onChange}
+                    iconChecked={<div data-automation-id='TEST_ICON_CHECKED'/>}
+                    iconUnchecked={<div data-automation-id='TEST_ICON_UNCHECKED'/>}
+                />
+            )
+            expect(renderer.select('TEST_ICON_CHECKED')).to.not.null;
+            expect(renderer.select('TEST_ICON_UNCHECKED')).to.be.null;
+        })
+    })
+
+    describe('render with displayIcons={false}', () => {
+        const {select, waitForDom} = clientRenderer.render(<Toogle checked displayIcon={false}/>)
+        it('should not render default unchecked icon', () => {
+            return waitForDom(() => expect(select('TOGGLE_UNCHECKED_ICON')).to.be.null);
+        })
+        it('should not render default checked icon', () => {
+            return waitForDom(() => expect(select('TOGGLE_CHECKED_ICON')).to.be.null);
         })
     })
 
