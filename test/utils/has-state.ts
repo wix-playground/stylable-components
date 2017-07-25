c8edd802090b6805db0f22cf39cfd7779f3a9608
Evgen Filatov <evgen.filatov@gmail.com>
2017-07-25 19:04:30 +0300

    using styles namespace for creating data attribute for tests

--- test/utils/has-state.ts
+++ test/utils/has-state.ts
@@ -1,10 +1,9 @@
 import {expect} from 'test-drive-react';
 export default function hasState(elem: Element | null, styles: any, name: string): void | never {
     if (!elem) {
-        throw new Error(`Element does not exists"`)
+        throw new Error(`Element does not exists"`);
     }
-    const map = styles.$stylesheet.cssStates({[name]: true});
-    const stylableName = Object.keys(map)[0];
-    expect(elem).to.have.attr(stylableName, 'true')
+    const attrName = 'data-' + styles.$stylesheet.namespace + '-' + name;
+    expect(elem).to.have.attr(attrName, 'true');
 }
 
