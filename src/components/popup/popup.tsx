737d58dc9d782d2dfca94dd71ac96343c304c745
WIXPRESS\danielstr <danielstr@wix.com>
2017-07-27 10:01:12 +0300

    added some layout tests

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -1,16 +1,35 @@
 import React = require('react');
+import ReactDOM = require('react-dom');
+import {CSSProperties} from "react";
 const style = require('./popup.st.css').default;
+
+export interface PositionPoint {
+    vertical: 'top' | 'bottom';
+    horizontal: 'left' | 'right';
+}
 
 export interface PopupProps {
     anchor: any;
     open?: boolean;
+    anchorPosition?: PositionPoint;
 }
 
 export class Popup extends React.Component<Partial<PopupProps>,{}> {
+    static defaultProps : Partial<PopupProps> = {
+        open: false,
+        anchorPosition: {vertical: 'bottom', horizontal: 'left'}
+    };
 
     render() {
+        if (!this.props.anchor) {
+            return null;
+        }
+
+        const rect = (this.props.anchor as HTMLElement).getBoundingClientRect();
+        const layout: CSSProperties = {position: 'absolute', top: rect[this.props.anchorPosition!.vertical], left:rect[this.props.anchorPosition!.horizontal]};
+
         return (
-            <div data-automation-id="POPUP" className={!this.props.open ? style.closed : ''}>
+            <div data-automation-id="POPUP" style={layout} className={!this.props.open ? style.closed : ''}>
                 {this.props.children}
             </div>
         );
--- test/components/popup.spec.tsx
+++ test/components/popup.spec.tsx
@@ -1,5 +1,6 @@
 import React = require('react');
 import { expect, ClientRenderer, simulate } from 'test-drive-react';
+import {selectDom} from 'test-drive';
 import {PopupDemo} from '../../demo/components/popup-demo';
 import {Popup} from '../../src/components/'
 
@@ -8,6 +9,7 @@ const container = 'POPUP_DEMO_DIV';
 
 describe('<Popup />', function () {
     const clientRenderer = new ClientRenderer();
+    const bodySelect = selectDom(document.body);
 
     afterEach(function () {
         clientRenderer.cleanup();
@@ -45,37 +47,73 @@ describe('<Popup />', function () {
         })
     });
 
-    it('displays the popup and renders its children if the open prop is given', function () {
-        let div;
-        const {select, waitForDom} = clientRenderer.render(
-            <div>
-                <div ref={(elem) => div = elem}>Anchor</div>
-                <Popup anchor={div} open={true}>
-                    <span data-automation-id="SPAN">Popup Body</span>
-                </Popup>
-            </div>
-        );
+    it('displays the popup and renders its children if the open prop is given', async function () {
+        let div: HTMLDivElement;
+        const { waitForDom} = clientRenderer.render(<div ref={(elem: HTMLDivElement) => div = elem}>Anchor</div>);
+
+        await waitForDom(() => {
+            expect(div).to.be.present();
+        });
+
+        clientRenderer.render(<Popup anchor={div!} open={true}>
+            <span data-automation-id="SPAN">Popup Body</span>
+        </Popup>);
 
         return waitForDom(() => {
-            expect(select(popup)).to.be.present();
-            expect(select(popup, 'SPAN')).to.be.present();
+            expect(bodySelect(popup)).to.be.present();
+            expect(bodySelect(popup, 'SPAN')).to.be.present();
         })
     });
 
-    it('renders the popup in the correct default position', function () {
+    it('renders the popup in the correct default position', async function () {
         let div: HTMLDivElement;
-        const {select, waitForDom} = clientRenderer.render(
-            <div>
-                <div ref={(elem: HTMLDivElement) => div = elem}>Anchor</div>
-                <Popup anchor={div!} open={true}>
-                    <span data-automation-id="SPAN">Popup Body</span>
-                </Popup>
-            </div>
-        );
+        const { waitForDom} = clientRenderer.render(<div ref={(elem: HTMLDivElement) => div = elem}>Anchor</div>);
+
+        await waitForDom(() => {
+            expect(div).to.be.present();
+        });
+
+        clientRenderer.render(<Popup anchor={div!} open={true}>
+            <span data-automation-id="SPAN">Popup Body</span>
+        </Popup>);
 
         return waitForDom(() => {
-            expect([select(popup), div]).to.be.horizontallyAligned('left');
-            expect([div, select(popup)]).to.be.inVerticalSequence();
+            expect([bodySelect(popup), div]).to.be.horizontallyAligned('left');
+            expect([div, bodySelect(popup)]).to.be.inVerticalSequence();
         })
-    })
+    });
+
+    describe('Layout tests', function () {
+
+        it('Anchor - vertical: bottom, horizontal: right', async function () {
+            let div: HTMLDivElement;
+            const { waitForDom} = clientRenderer.render(<div ref={(elem: HTMLDivElement) => div = elem}>Anchor</div>);
+
+            await waitForDom(() => {expect(div).to.be.present();});
+            clientRenderer.render(<Popup anchor={div!} anchorPosition={{vertical: 'bottom', horizontal: 'right'}} open={true}>
+                <span data-automation-id="SPAN">Popup Body</span>
+            </Popup>);
+
+            return waitForDom(() => {
+                expect([div, bodySelect(popup)]).to.be.inVerticalSequence();
+                expect([div, bodySelect(popup)]).to.be.inHorizontalSequence();
+            })
+        });
+
+        it('Anchor - vertical: top, horizontal: left', async function () {
+            let div: HTMLDivElement;
+            const { waitForDom} = clientRenderer.render(<div ref={(elem: HTMLDivElement) => div = elem}>Anchor</div>);
+            
+            await waitForDom(() => {expect(div).to.be.present();});
+            clientRenderer.render(<Popup anchor={div!} anchorPosition={{vertical: 'top', horizontal: 'left'}} open={true}>
+                <span data-automation-id="SPAN">Popup Body</span>
+                <div>some more stuff</div>
+            </Popup>);
+
+            return waitForDom(() => {
+                expect([bodySelect(popup), div]).to.be.horizontallyAligned('left');
+                expect([bodySelect(popup), div]).to.be.verticallyAligned('top');
+            })
+        });
+    });
 });
