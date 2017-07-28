bfb9adb2f9ef5bc973e2c7bebfab7e1fd02984e3
Evgen Filatov <evgen.filatov@gmail.com>
2017-07-28 15:17:52 +0300

    added stateless component support to @rtl

--- src/common/rtl.tsx
+++ src/common/rtl.tsx
@@ -1,21 +1,42 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types'
 
-export default function rtl<T extends React.ComponentClass<any>>(Base: T): T {
-    class Component extends React.Component {
-        static displayName = Base.displayName
-        static contextTypes = {
-            rtl: PropTypes.bool.isRequired
-        }
-        static childContextTypes = {
-            rtl: PropTypes.bool.isRequired
-        }
-        render() {
-            return React.createElement(Base, {
-                rtl: this.context.rtl || false,
-                ...this.props
-            });
+const contextTypes = {
+    rtl: PropTypes.bool.isRequired
+};
+
+export function rtl<T extends React.ComponentClass<any>>(Base: T): T
+export function rtl<T extends React.SFC>(Base: T): React.SFC
+
+export default function rtl(Base: any): any {
+    if (Base.prototype.render) {
+        return class Component extends React.Component {
+            static displayName = Base.name
+            static contextTypes = contextTypes
+            render() {
+                return React.createElement(Base, {
+                    rtl: this.context.rtl || false,
+                    ...this.props
+                })
+            }
         }
     }
-    return Component as T
+
+    let warnShown = false
+
+    const SLComponent: React.SFC = (props: any, context: any) => {
+        if (context) {
+            context = {}
+            if (!warnShown) {
+                console.error(`Warning: Stateless component \`${Base.name}\` being used as function, which make \`context\` unreachable`)
+                warnShown = true
+            }
+        }
+        return Base({
+            rtl: context.rtl || false,
+            ...props
+        });
+    }
+    SLComponent.contextTypes = contextTypes;
+    return SLComponent
 }
