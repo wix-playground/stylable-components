0e7301c35c72dab6ab0356c2f73040c94a8d0d00
Kieran Williams <kieranw@wix.com>
2017-07-30 18:28:54 +0300

    Removed mobx from demo

--- demo/components/date-picker-demo.tsx
+++ demo/components/date-picker-demo.tsx
@@ -1,15 +1,11 @@
 import * as React from 'react';
 import { DatePicker, DatePickerProps } from '../../src';
-import {observable, action} from 'mobx';
-import {observer} from 'mobx-react';
 
 export interface DatePickerDemoProps extends Partial<DatePickerProps> {}
 
-@observer
 export class DatePickerDemo extends React.Component<DatePickerDemoProps, {}> {
-    @observable date: Date = this.props.value ? this.props.value : new Date();
+    date: Date = this.props.value ? this.props.value : new Date();
 
-    @action
     onChange = (updatedDate: Date): void => {
         this.date = updatedDate;
     };
