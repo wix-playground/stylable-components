9d2328f819403f1a20891b04a9baa2176fa7db08
Kieran Williams <kieranw@wix.com>
2017-08-01 16:53:28 +0300

    Improved variable names

--- src/common/date-helpers.ts
+++ src/common/date-helpers.ts
@@ -1,10 +1,10 @@
-export function getDayNames (firstDay: number = 0, nameLength: string = 'short', locale: string = 'en-US'): Array<string> {
+export function getDayNames (startingDay: number = 0, nameLength: string = 'short', locale: string = 'en-US'): Array<string> {
     // Days start from Sunday (Sunday = 0, Monday = 1, etc.)
     const listOfDayNames: Array<string> = [];
 
     // For each of the seven days of the week
-    for (let i = 0; i < 7; i++) {
-        let dayName = new Date(1970, 5, i + firstDay).toLocaleString(locale, { weekday: nameLength });
+    for (let day = 0; day < 7; day++) {
+        let dayName = new Date(1970, 5, day + startingDay).toLocaleString(locale, { weekday: nameLength });
         listOfDayNames.push(dayName);
     }
 
@@ -15,8 +15,8 @@ export function getMonthNames (locale: string = 'en-US'): Array<string> {
     const listOfMonthNames: Array<string> = [];
 
     // For each of the twelve months of the year
-    for (let i = 0; i < 12; i++) {
-        let monthName = new Date(1970, i, 1).toLocaleString(locale, { month: 'long' });
+    for (let month = 0; month < 12; month++) {
+        let monthName = new Date(1970, month, 1).toLocaleString(locale, { month: 'long' });
         listOfMonthNames.push(monthName);
     }
     return listOfMonthNames;
@@ -36,13 +36,13 @@ export function getLastDayOfMonth (date: Date): number {
     return new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
 }
 
-export function getNumOfPreviousDays (date: Date, firstDay: number = 0): number {
+export function getNumOfPreviousDays (date: Date, startingDay: number = 0): number {
     // Days start from Sunday (Sunday = 0, Monday = 1, etc.)
-    const previousDays: number = (new Date(date.getFullYear(), date.getMonth(), 1).getDay() - firstDay) + 7;
+    const previousDays: number = (new Date(date.getFullYear(), date.getMonth(), 1).getDay() - startingDay) + 7;
     return previousDays > 6 ? previousDays - 7 : previousDays;
 }
 
-export function getNumOfFollowingDays (date: Date, firstDay: number = 0): number {
-    const followingDays = (6 - getLastDayOfMonth(date)) + firstDay;
+export function getNumOfFollowingDays (date: Date, startingDay: number = 0): number {
+    const followingDays = (6 - getLastDayOfMonth(date)) + startingDay;
     return followingDays > 6 ? followingDays - 7 : followingDays;
 }
