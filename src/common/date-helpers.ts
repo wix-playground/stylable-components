2378f4a1586e3d6623b664a09e466d0cb939b3ac
Kieran Williams <kieranw@wix.com>
2017-08-02 17:28:03 +0300

    Added const and removed trailing zeroes

--- src/common/date-helpers.ts
+++ src/common/date-helpers.ts
@@ -4,7 +4,7 @@ export function getDayNames (startingDay: number = 0, nameLength: string = 'shor
 
     // For each of the seven days of the week
     for (let day = 0; day < 7; day++) {
-        let dayName = new Date(Date.UTC(1970, 5, day + startingDay, 0, 0, 0)).toLocaleString(locale, { weekday: nameLength });
+        const dayName = new Date(Date.UTC(1970, 5, day + startingDay)).toLocaleString(locale, { weekday: nameLength });
         listOfDayNames.push(dayName);
     }
 
@@ -16,29 +16,29 @@ export function getMonthNames (locale: string = 'en-US'): Array<string> {
 
     // For each of the twelve months of the year
     for (let month = 0; month < 12; month++) {
-        let monthName = new Date(Date.UTC(1970, month, 1, 0, 0, 0)).toLocaleString(locale, { month: 'long' });
+        const monthName = new Date(Date.UTC(1970, month, 1)).toLocaleString(locale, { month: 'long' });
         listOfMonthNames.push(monthName);
     }
     return listOfMonthNames;
 }
 
 export function getMonthFromOffset (date: Date, offset: number): Date {
-    return new Date(Date.UTC(date.getFullYear(), date.getMonth() + offset, 1, 0, 0, 0));
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth() + offset, 1));
 }
 
 export function getDaysInMonth (date: Date): number {
     // Important: the '0' in the day category "rolls back" the daysArray to the start of the previous month
     // so we add a month to the daysArray in order to get the number of days for the intended month
-    return new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0, 0, 0, 0)).getDate();
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0)).getDate();
 }
 
 export function getLastDayOfMonth (date: Date): number {
-    return new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0, 0, 0, 0)).getDay();
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth() + 1, 0)).getDay();
 }
 
 export function getNumOfPreviousDays (date: Date, startingDay: number = 0): number {
     // Days start from Sunday (Sunday = 0, Monday = 1, etc.)
-    const previousDays: number = (new Date(Date.UTC(date.getFullYear(), date.getMonth(), 1, 0, 0, 0)).getDay() - startingDay) + 7;
+    const previousDays: number = (new Date(Date.UTC(date.getFullYear(), date.getMonth(), 1)).getDay() - startingDay) + 7;
     return previousDays > 6 ? previousDays - 7 : previousDays;
 }
 
