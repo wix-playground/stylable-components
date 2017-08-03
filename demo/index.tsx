31019220a31d5cb4ab146fd684a07074c9ecfbb4
Evgen Filatov <evgen.filatov@gmail.com>
2017-08-03 19:06:12 +0300

    added support of mobile for demo page

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,6 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { ComponentsDemo } from './components-demo';
 
+const meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width, initial-scale=1, maximum-scale=1';
+document.head.appendChild(meta);
+
 const rootContainer = document.createElement('div');
 document.body.appendChild(rootContainer);
 ReactDOM.render(<ComponentsDemo />, rootContainer);
