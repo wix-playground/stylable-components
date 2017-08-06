9d43b986b0b3ba8e1f4a83aeda8fe31251d58424
Lior Zisman <liorzisman@gmail.com>
2017-08-06 16:45:16 +0300

    <TreeView /> - Stage 3 - Keyboard Navigation (#52)
    * implementing basic functinality of phase 1 of the tree view, tree item, default rendering
    
    * refactoring tree item rendering type
    
    * flattening labels for testing purposes
    
    * double clicks is double the fun
    
    * resolving tricky issue with onClick happening twice
    
    * refactoring interfaces. still facing double invoke to one click
    
    * resolved double invocation of onClick
    
    * formatting
    
    * polished for pr
    
    * added style for demo purposes
    
    * trying createElement to render observer wrapped functional component
    
    * trying to make things react to mobx
    
    * expand and collapse
    
    * expanded items close only if they are already selected
    
    * refactoring
    
    * improved demo greatly
    
    * PR fixes including - demo improvements, indentation
    
    * demo folder structure adjusted
    
    * protection for ssr
    
    * making progress towards better rendering performances
    
    * each item now renders seperately (rathar than by the container)
    
    * one step closer
    
    * wip
    
    * exports, default values
    
    * moving to es6 map
    
    * moved to use es6 for state management. tree items not getting reactive props still
    
    * factoring
    
    * items render seperately
    
    * cleaning up
    
    * cleanup 2
    
    * ready for pr
    
    * exporting type for stateMap
    
    * default added
    
    * refactored interfaces to reflect actual use cases. improved tests to comply
    
    * resolving teh key error thing
    
    * fixed the key issue. avi vahl is the best
    
    * fixed so now items are totally toggle-able
    
    * changed demo to use mobx
    
    * changed to use .ref
    
    * basics of keyboard handling
    
    * implemented prop for focused item
    
    * added to specific item state if its focused or not
    
    * trying out a method to manage order of nodes
    
    * moving to use parent for navigation purposes
    
    * down key works
    
    * up key works
    
    * fixed types
    
    * works
    
    * keyboard navigationes
    
    * keyboard navigations still need work but works now
    
    * keyboard navigation wip
    
    * escape to unfocus
    
    * HOME key
    
    * END key
    
    * finished with weird cases of up and down keyboard clicks
    
    * last few keyboard handling for left and right special cases
    
    * adjusted a test
    
    * need to refine stuff
    
    * removed focus outline
    
    * fixed bug where child-less nodes were considered expanded
    
    * fixed an issue with trying to access undefined parent children
    
    * using lazy state init
    
    * added some styling. waiting for svg from ux people
    
    * added full beahvior for left and right keys
    
    * breaking tests to handle the expand only on icon feature
    
    * wip
    
    * final adjustments to icon and label functionality. cleaning up tests
    
    * progress in cleanup
    
    * cleanup completed
    
    * uglified
    
    * added strict to tests. added actions to tree view so it works
    
    * added webpack config entry for the mobx config
    
    * in process of refactoring
    
    * refactoring tree utilities
    
    * keycodes
    
    * adjustments
    
    * unit testing tree data traversal
    
    * fixed typo
    
    * adjustments

---
+++ src/components/tree-view/tree-util.ts
@@ -0,0 +1,54 @@
+import { TreeItemData } from './tree-view';
+import { ParentsMap, TreeStateMap } from './tree-view';
+
+export function getPreviousItem(dataSource: Object[], item: TreeItemData, stateMap: TreeStateMap, parentsMap: ParentsMap): TreeItemData {
+    const parent = parentsMap.get(item);
+
+    const siblings = parent ? parent.children! : dataSource;
+
+    const itemIdx = siblings.indexOf(item);
+    if (itemIdx === 0) return parent ? parent : item;
+
+    const prevSibling = siblings[itemIdx - 1] as TreeItemData;
+    const prevSiblingState = stateMap.getItemState(prevSibling);
+
+    if (prevSiblingState.isExpanded && prevSibling.children!.length ) {
+        return prevSibling.children![prevSibling.children!.length - 1];
+    } else {
+        return prevSibling;
+    }
+}
+
+export function getNextItem(dataSource: Object[], item: TreeItemData, stateMap: TreeStateMap, parentsMap: ParentsMap): TreeItemData {
+    const itemState = stateMap.getItemState(item);
+
+    if (itemState.isExpanded && item.children) {
+        return item.children![0];
+    } else {
+        const parent = parentsMap.get(item);
+        const siblings = parent ? parent.children! : dataSource;
+        const itemIdx = siblings.indexOf(item);
+        return itemIdx !== siblings.length - 1 ? siblings[itemIdx + 1] as TreeItemData: getNextParentSibling(item, parent, parentsMap);
+    }
+}
+
+export function getLastAvailableItem(lastChild: TreeItemData, stateMap: TreeStateMap): TreeItemData {
+    if (stateMap.getItemState(lastChild).isExpanded && lastChild.children) {
+        return getLastAvailableItem(lastChild.children[lastChild.children.length - 1], stateMap);
+    } else {
+        return lastChild;
+    }
+
+}
+
+export function getNextParentSibling(item: TreeItemData, parent: TreeItemData | undefined, parentsMap: ParentsMap): TreeItemData {
+    if (!parent) {
+        return item;
+    } else {
+        const grandParent = parentsMap.get(parent);
+        if (!grandParent) return item;
+        const grandParentChildren = grandParent!.children!;
+        const parentIdx = grandParentChildren.indexOf(parent);
+        return parentIdx !== grandParentChildren.length - 1 ? grandParentChildren[parentIdx + 1] : item;
+    }
+}
