70540cd7e7ab2b60c560c49acf6aecfe2b6795c7
Lior Zisman <liorzi@wix.com>
2017-08-08 16:59:52 +0300

    implementing basics for dropdown label and callback

--- src/components/drop-down/drop-down.tsx
+++ src/components/drop-down/drop-down.tsx
@@ -1,7 +1,31 @@
 import * as React from 'react';
 
-export class DropDown extends React.Component<{}, {}> {
+export interface DropDownLabelProps {
+    label: string;
+    onClick?: React.EventHandler<any>;
+}
+
+export const DropDownLabel: React.SFC<DropDownLabelProps> = props => {
+  return (
+      <div data-automation-id="DROP_DOWN_LABEL" onClick={props.onClick}>{props.label}</div>
+  );
+};
+
+export interface DropDownItem {
+    label: string;
+}
+
+export interface DropDownProps {
+    selectedItem: DropDownItem;
+    onLabelClick?: React.EventHandler<any>;
+}
+
+export class DropDown extends React.Component<DropDownProps, {}> {
     public render() {
-        return <div data-automation-id="DROP_DOWN">DropDown</div>;
+        return (
+            <div data-automation-id="DROP_DOWN">
+                <DropDownLabel label={this.props.selectedItem.label} onClick={this.props.onLabelClick} />
+            </div>
+        );
     }
 }
--- test/components/drop-down.spec.tsx
+++ test/components/drop-down.spec.tsx
@@ -3,13 +3,30 @@ import { ClientRenderer, expect, simulate, sinon, waitFor } from 'test-drive-rea
 import { DropDown } from '../../src';
 
 const dropDown = 'DROP_DOWN';
+const label = dropDown + '_LABEL';
 
 describe('<DropDown />', () => {
     const clientRenderer = new ClientRenderer();
     afterEach(() => clientRenderer.cleanup());
 
+    const item = {
+        label: 'Test'
+    };
+
     it('renders to the screen', () => {
-       const { select, waitForDom } = clientRenderer.render(<DropDown />);
+       const { select, waitForDom } = clientRenderer.render(<DropDown selectedItem={item} />);
        return waitForDom(() => expect(select(dropDown)).to.be.present());
+    });
+
+    it('has default text initially', () => {
+        const { select, waitForDom } = clientRenderer.render(<DropDown selectedItem={item}/>);
+        return waitForDom(() => expect(select(dropDown, label)).to.be.present());
+    });
+
+    it('invokes the onClick when dropdown label is clicked', () => {
+        const onClick = sinon.spy();
+        const { select } = clientRenderer.render(<DropDown selectedItem={item} onLabelClick={onClick}/>);
+        simulate.click(select(dropDown, label));
+        return waitFor(() => expect(onClick).to.have.been.calledOnce);
     });
 });
