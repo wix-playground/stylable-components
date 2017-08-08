56ece56f1c5b604d6efb3473f93ae8b48c6dae5f
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2017-08-08 16:46:51 +0300

    simulateKeyInput test utility function is now more simple;

--- test/utils/simulate-key-input.ts
+++ test/utils/simulate-key-input.ts
@@ -1,16 +1,9 @@
 import {simulate} from 'test-drive-react';
 
-const inputs = new WeakSet();
-
 export default function simulateKeyInput(
     input: HTMLInputElement,
     value: string
 ) {
-    if (inputs.has(input)) {
-        input.value += value;
-    } else {
-        input.value = value;
-        inputs.add(input);
-    }
+    input.value += value;
     simulate.change(input);
 }
