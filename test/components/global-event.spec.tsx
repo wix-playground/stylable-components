b1206e2fca14d6850089eb36aa3fdd82abffe261
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2017-08-09 18:49:11 +0300

    added proper tests for mount/unmount of global-event component;
    (cherry picked from commit 46285f3c66b752a356bbc1eabe8d4c863cf5ad06)

--- test/components/global-event.spec.tsx
+++ test/components/global-event.spec.tsx
@@ -1,26 +1,58 @@
 import * as React from 'react';
+import {render, unmountComponentAtNode} from 'react-dom';
+import {SinonMock} from 'sinon';
 import { ClientRenderer, expect, simulate, sinon } from 'test-drive-react';
 import GlobalEvent from '../../src/common/global-event';
 
 describe('<GlobalEvent />', () => {
     const clientRenderer = new ClientRenderer();
+    let emitterMock: SinonMock;
+    let container: HTMLElement;
+
+    beforeEach(() => emitterMock = sinon.mock(window));
+    beforeEach(() => {
+        container = document.createElement('div');
+        container.setAttribute('style', 'position: fixed; top: 0; left: 0; right: 0;');
+        document.body.appendChild(container);
+    });
+
+    afterEach(() => emitterMock.restore());
+    afterEach(() => document.body.removeChild(container));
+
     afterEach(() => clientRenderer.cleanup());
 
-    it('should exist', () => {
-        expect(GlobalEvent).to.exist;
-    });
+    describe('mount', () => {
+        it('should add an event listener on window object when mounted', async () => {
+            const event = 'click';
+            const handler = () => {};
 
-    describe('mounting', () => {
-        it('should call the handler with corresponding event', async () => {
-            const handler = sinon.spy();
-            const {waitForDom} = clientRenderer.render(
-                <GlobalEvent event="click" handler={handler} />
+            emitterMock.expects('addEventListener').once();
+
+            render(
+                <GlobalEvent event={event} handler={handler} />,
+                container
             );
 
-            await waitForDom(() => {
-                simulate.click(document.body);
-                expect(handler).to.have.been.called;
-            });
+            emitterMock.verify();
         });
     });
+
+    describe('unmount', () => {
+        it('should remove an event listener on window object when unmounted', async () => {
+            const event = 'click';
+            const handler = () => {};
+
+            emitterMock.expects('removeEventListener').once();
+
+            render(
+                <GlobalEvent event={event} handler={handler} />,
+                container
+            );
+
+            unmountComponentAtNode(container);
+
+            emitterMock.verify();
+        });
+    });
+
 });
