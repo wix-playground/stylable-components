16cdda16bd75a10ebb89d4a947cdb192b5af1bc5
WIXPRESS\danielstr <danielstr@wix.com>
2017-08-14 12:25:24 +0300

    extracted the relevant tests and added a few more

--- test/components/portal.spec.tsx
+++ test/components/portal.spec.tsx
@@ -1,8 +1,10 @@
+import sleep from '../../src/common/sleep';
 import React = require('react');
+import ReactDOM = require('react-dom');
 import {selectDom} from 'test-drive';
 import { ClientRenderer, expect, waitFor } from 'test-drive-react';
 import {Portal} from '../../src';
-import sleep from '../../src/common/sleep';
+import {PortalFixture} from '../fixtures/portal-fixture';
 
 const portal = 'PORTAL';
 
@@ -12,7 +14,7 @@ describe('<Portal />', function() {
 
     afterEach(function() {clientRenderer.cleanup(); });
 
-    it('displays the portal and renders its children if the open prop is given', function() {
+    it('displays the portal and renders its children if the open prop is given', async function() {
         clientRenderer.render(
             <Portal
                 open={true}
@@ -20,37 +22,56 @@ describe('<Portal />', function() {
                 <span data-automation-id="SPAN">Portal Body</span>
             </Portal>);
 
-        return waitFor(() => {
+        await waitFor(() => {
             expect(bodySelect(portal)).to.be.present();
             expect(bodySelect(portal, 'SPAN')).to.be.present();
         });
     });
 
-    it('does not add the portal to the DOM if open is false', function() {
+    it('does not add the portal to the DOM if open is false', async function() {
         clientRenderer.render(
             <Portal
                 open={false}
             >
                 <span data-automation-id="SPAN">Portal Body</span>
             </Portal>);
-
-        return waitFor(() => {
+        await sleep(500);
+        await waitFor(() => {
             expect(bodySelect(portal)).to.not.exist;
             expect(bodySelect(portal, 'SPAN')).to.not.exist;
         });
     });
 
-    // it('removes the component when unmounting', async function() {
-    //
-    //     clientRenderer.render(
-    //         <Portal
-    //             open={true}
-    //         >
-    //             <span data-automation-id="SPAN">Popup Body</span>
-    //         </Portal>);
+    it('removes the portal if the props update', async function() {
+        let comp: PortalFixture | null;
+        clientRenderer.render(<PortalFixture ref={ref => comp = ref}/>);
 
-        // await waitFor(() => {expect(bodySelect(popup)).to.be.present(); });
-        // ReactDOM.unmountComponentAtNode(bodySelect(popup)!.parentElement!);
-        // return waitFor(() => {expect(bodySelect(popup)).to.not.exist; });
-    // });
+        await waitFor(() => {
+            expect(bodySelect(portal)).to.be.present();
+            expect(bodySelect('FIXTURE_CHILDREN')).to.be.present();
+        });
+        comp!.toggle();
+        await waitFor(() => {
+            expect(bodySelect(portal)).to.not.exist;
+            expect(bodySelect(portal, 'FIXTURE_CHILDREN')).to.not.exist;
+        });
+    });
+
+    it('removes the component when unmounting', async function() {
+        const div = document.body.appendChild(document.createElement('div'));
+        clientRenderer.render(
+            <Portal
+                open={true}
+            >
+                <span data-automation-id="SPAN">Popup Body</span>
+            </Portal>, div);
+
+        await waitFor(() => expect(bodySelect(portal)).to.be.present());
+        ReactDOM.unmountComponentAtNode(div);
+        document.body.removeChild(div);
+        await waitFor(() => {
+            expect(bodySelect(portal)).to.not.exist;
+            expect(bodySelect('SPAN')).to.not.exist;
+        });
+    });
 });
