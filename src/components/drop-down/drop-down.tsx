9990c6c0c37bfe0a45cdb20d6982dab52a0f0373
Lior Zisman <liorzisman@gmail.com>
2017-08-15 10:45:18 +0300

    <DropDown /> Phase 2 - Integration of SelectionList to display items (#164)
    * dropdown folders, tests, render test
    
    * implementing basics for dropdown label and callback
    
    * added some styling to phase 1
    
    * liniting
    
    * reducing diff
    
    * writing tests for the list integration. missing data automation ids. going to open PR on selection list
    
    * list wired
    
    * wip
    
    * whoops
    
    * pr changes
    
    * fixed
    
    * adjusted tests text
    
    * added hr
    
    * adusted interface for dropdownprops
    
    * optionals are the best
    
    * dropdown reacts to clicks on list that open and closes, no pop up just yet

--- src/components/drop-down/drop-down.tsx
+++ src/components/drop-down/drop-down.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { SBComponent, SBStateless } from 'stylable-react-component/dist/stylable-react';
 import { root } from 'wix-react-tools';
 import { CaretDown } from './drop-down-icons';
+import { SelectionList } from '../selection-list';
 import style from './drop-down.st.css';
 
 export interface DropDownInputProps {
@@ -10,12 +11,34 @@ export interface DropDownInputProps {
 }
 
 export const DropDownInput: React.SFC<DropDownInputProps> = SBStateless(props => {
-  return (
-      <div data-automation-id="DROP_DOWN_INPUT" onClick={props.onClick} className="drop-down-input">
-          <span className="label">{props.selectedItem ? props.selectedItem.label : 'Default Text'}</span>
-          <CaretDown className="caret" />
-      </div>
-  );
+    return (
+        <div data-automation-id="DROP_DOWN_INPUT" onClick={props.onClick} className="drop-down-input">
+            <span className="label">{props.selectedItem ? props.selectedItem.label : 'Default Text'}</span>
+            <CaretDown className="caret" />
+        </div>
+    );
+}, style);
+
+export interface DropDownListProps {
+    open: boolean;
+    items?: DropDownItem[];
+    selectedItem?: DropDownItem;
+    onItemClick?: (item: string) => void;
+}
+
+export const DropDownList: React.SFC<DropDownListProps> = SBStateless(props => {
+    if (!props.open) { return null; }
+
+    return (
+        <div data-automation-id="DROP_DOWN_LIST">
+            <SelectionList
+                className="drop-down-list"
+                dataSource={props.items!.map((item: DropDownItem) => item.label)}
+                value={props.selectedItem && props.selectedItem.label}
+                onChange={props.onItemClick!}
+            />
+        </div>
+    );
 }, style);
 
 export interface DropDownItem {
@@ -24,11 +47,22 @@ export interface DropDownItem {
 
 export interface DropDownProps extends React.HTMLAttributes<HTMLDivElement> {
     selectedItem?: DropDownItem;
-    onInputClick?: React.EventHandler<React.MouseEvent<HTMLDivElement>>;
+    onInputClick?: () => void;
+    open?: boolean;
+    items?: DropDownItem[];
+    onItemClick?: (item: DropDownItem) => void;
 }
 
 @SBComponent(style)
 export class DropDown extends React.Component<DropDownProps, {}> {
+
+    public static defaultProps: DropDownProps = { items: [], onItemClick: () => {}, onInputClick: () => {} };
+
+    public onItemClick = (item: string) => {
+        this.props.onInputClick!();
+        this.props.onItemClick!(this.props.items!.filter((elem: DropDownItem) => elem.label === item)[0]);
+    };
+
     public render() {
         const rootProps = root(this.props, {
             'data-automation-id': 'DROP_DOWN',
@@ -36,8 +70,14 @@ export class DropDown extends React.Component<DropDownProps, {}> {
         });
 
         return (
-            <div {...rootProps}>
+            <div data-automation-id="DROP_DOWN" {...rootProps}>
                 <DropDownInput selectedItem={this.props.selectedItem} onClick={this.props.onInputClick} />
+                <DropDownList
+                    selectedItem={this.props.selectedItem}
+                    items={this.props.items}
+                    open={!!this.props.open}
+                    onItemClick={this.onItemClick}
+                />
             </div>
         );
     }
