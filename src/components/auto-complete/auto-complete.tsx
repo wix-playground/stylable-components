fe298eeaf40eced9efa91fc4c88e5419cddbaf34
liorzisman <liorzisman@gmail.com>
2017-08-17 12:42:49 +0300

    autocomp phase 1

--- src/components/auto-complete/auto-complete.tsx
+++ src/components/auto-complete/auto-complete.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { SBComponent } from 'stylable-react-component/dist/stylable-react';
 import { OptionList } from '../../../lib/components/selection-list/selection-list';
+import { root } from 'wix-react-tools';
 import style from './auto-complete.st.css';
 
 export interface AutoCompleteProps extends OptionList {
     value?: string;
-    // onChange?: React.EventHandler<React.ChangeEvent<HTMLInputElement>> | undefined;
     onChange?: (value: string) => void;
     style?: any;
     className?: string;
@@ -17,8 +17,13 @@ export class AutoComplete extends React.Component<AutoCompleteProps, {}> {
     public static defaultProps: AutoCompleteProps = { dataSource: [], onChange: () => {}, value: '' };
 
     public render() {
+        const rootProps = root(this.props, {
+            'data-automation-id': 'AUTO_COMPLETE',
+            className: 'auto-complete',
+        }) as React.HtmlHTMLAttributes<HTMLDivElement>;
+
         return (
-            <div data-automation-id="AUTO_COMPLETE" className="auto-complete">
+            <div {...rootProps}>
                 <input
                     data-automation-id="AUTO_COMPLETE_INPUT"
                     type="text"
