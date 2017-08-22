a967db70e2800d9324fecf00adaf8451b4d76d88
Oleksii Kirgizov <oleksiik@Oleksiik-MacBook-Pro.local>
2017-08-22 15:30:43 +0300

    changes to the theme

--- demo/components/number-input.demo.tsx
+++ demo/components/number-input.demo.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {NumberInput} from '../../src/components/number-input';
-import * as style from '../wix-theme/theme.st.css';
 
 export class NumberInputDemo extends React.Component<{}, {value?: number}> {
 
