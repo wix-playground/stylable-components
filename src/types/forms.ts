593f29f540f32cf9d59a4c4292728d642e34b40a
Jiri Tobisek <jiri@wix.com>
2017-08-24 12:00:12 +0300

    FormInput -> FormInputProps

--- src/types/forms.ts
+++ src/types/forms.ts
@@ -1,6 +1,6 @@
 import {ChangeEvent} from "./events";
 
-export interface FormInput<T, S = any> {
+export interface FormInputProps<T, S = any> {
     value: T;
     onChange: (event: ChangeEvent<T>) => void;
     onInput?: (event: ChangeEvent<S>) => void;
