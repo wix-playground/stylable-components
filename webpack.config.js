64b382d950781d8adf7afd39d2c77ec89415b144
Maksym Kramarenko <maksym@wix.com>
2017-08-28 19:23:12 +0300

    Clean webpack.config.js up

--- webpack.config.js
+++ webpack.config.js
@@ -31,9 +31,6 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
     },
-    devServer: {
-        disableHostCheck: true
-    },
     output: {
         filename: '[name].js',
         pathinfo: true
