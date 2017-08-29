69817b8cabf4005b96b2e697d0644b7d0f52ac21
Maksym Kramarenko <maksym@wix.com>
2017-08-29 16:41:44 +0300

    See what is the problem with tests on Win81 ie11

--- test/utils/simulate-mouse-event.ts
+++ test/utils/simulate-mouse-event.ts
@@ -1,13 +1,36 @@
+export interface Options {
+    clientX?: number;
+    clientY?: number;
+}
+
 export function simulateMouseEvent(
     element: Element,
     eventType: string,
-    options?: object
+    options?: Options
 ): void {
-    setTimeout(function() {
-        throw new Error('eventTypeeventType ' + eventType);
-    }, 0);
-    element.dispatchEvent(new MouseEvent(
-        eventType,
-        options as any as EventInit
-    ));
+    const event = document.createEvent('MouseEvents');
+    if (event.initMouseEvent) {
+        event.initMouseEvent(
+            eventType,
+            true,
+            true,
+            window,
+            0,
+            0,
+            0,
+            options!.clientX || 0,
+            options!.clientY || 0,
+            false,
+            false,
+            false,
+            false,
+            0,
+            null
+        );
+    } else {
+        element.dispatchEvent(new MouseEvent(
+            eventType,
+            options as any as EventInit
+        ));
+    }
 }
