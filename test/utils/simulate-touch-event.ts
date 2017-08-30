6054f54d01e644b1999e2c8170767662a09de16a
Maksym Kramarenko <maksym@wix.com>
2017-08-30 12:46:55 +0300

    See what is the problem with tests on Win81 ie11

--- test/utils/simulate-touch-event.ts
+++ test/utils/simulate-touch-event.ts
@@ -13,12 +13,31 @@ export function simulateTouchEvent(
         clientY: options.y
     };
 
-    element.dispatchEvent(new TouchEvent(
-        eventType,
-        {
-            touches: [touchObj],
-            targetTouches: [],
-            changedTouches: [touchObj]
-        } as any as TouchEventInit
-    ));
+    const touchEventInit: any = {
+        touches: [touchObj],
+        targetTouches: [touchObj],
+        changedTouches: [touchObj]
+    };
+
+    // element.dispatchEvent(new TouchEvent(
+    //     eventType,
+    //     {
+    //         touches: [touchObj],
+    //         targetTouches: [touchObj],
+    //         changedTouches: [touchObj]
+    //     } as any as TouchEventInit
+    // ));
+    let event: any;
+
+    if (TouchEvent) {
+        touchEventInit.bubbles = true;
+        touchEventInit.cancelable = true;
+        event = new TouchEvent(eventType, touchEventInit);
+    } else {
+        event = new CustomEvent(eventType, {bubbles: true, cancelable: true});
+        Object.keys(touchEventInit)
+            .forEach(property => event[property] = touchEventInit[property]);
+    }
+
+    element.dispatchEvent(event);
 }
