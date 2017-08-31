fefcd700a1b5926985a94de5514f1de4c968bd2e
Evgen Filatov <evgen.filatov@gmail.com>
2017-08-31 11:21:45 +0300

    hide notify alert

--- src/utils/notify-screen-reader.ts
+++ src/utils/notify-screen-reader.ts
@@ -2,6 +2,8 @@ let notifyAlert: HTMLElement;
 export function notifyScreenReader(text: string) {
     if (!notifyAlert) {
         notifyAlert = document.createElement('p');
+        notifyAlert.style.position = 'absolute';
+        notifyAlert.style.top = '-100px';
         notifyAlert.setAttribute('role', 'alert');
         notifyAlert.innerHTML = text;
         document.body.appendChild(notifyAlert);
