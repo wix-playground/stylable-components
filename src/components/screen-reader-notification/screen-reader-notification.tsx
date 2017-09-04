4ce4c6c3ae06807fc50a94e83e01ac1644f3c3d1
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-04 18:20:12 +0300

    wrapper screen-reader-notification into stylable

--- src/components/screen-reader-notification/screen-reader-notification.tsx
+++ src/components/screen-reader-notification/screen-reader-notification.tsx
@@ -1,15 +1,17 @@
 import * as React from 'react';
+import {Stylesheet} from 'stylable';
+import {SBStateless} from 'stylable-react-component';
+import styles from './screen-reader-notification.st.css';
 
-export function ScreenReaderNotification({children}: {children: string}) {
-    return (
-        <p
-            style={{
-                position: 'fixed',
-                bottom: '100%',
-                left: '100%'
-            }}
-            role="alert"
-            children={children}
-        />
-    );
-}
+export type Stylesheet = Stylesheet;
+export const ScreenReaderNotification = SBStateless(
+    ({children}: {children: React.ReactNode}) => {
+        return (
+            <p
+                role="alert"
+                children={children}
+            />
+        );
+    },
+    styles
+);
