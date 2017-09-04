344104d69b6bfb1c204e516ef25be60c80674cc0
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-04 14:42:09 +0300

    fixed node test

--- src/utils/create-screen-reader-notifier.ts
+++ src/utils/create-screen-reader-notifier.ts
@@ -7,21 +7,21 @@ let notifyAlert: any;
 let listeners = 0;
 
 export function createScreenReaderNotifier(): ScreenReaderNotifier {
-    if (!notifyAlert) {
-        notifyAlert = document.createElement('p');
-        notifyAlert.style.position = 'absolute';
-        notifyAlert.style.bottom = '100%';
-        notifyAlert.setAttribute('role', 'alert');
-        document.body.appendChild(notifyAlert);
-    }
     listeners ++;
 
     function notify(text: string) {
+        if (!notifyAlert) {
+            notifyAlert = document.createElement('p');
+            notifyAlert.style.position = 'absolute';
+            notifyAlert.style.bottom = '100%';
+            notifyAlert.setAttribute('role', 'alert');
+            document.body.appendChild(notifyAlert);
+        }
         notifyAlert.innerHTML = text;
     }
     function remove() {
         listeners --;
-        if (listeners === 0) {
+        if (listeners === 0 && notifyAlert) {
             document.body.removeChild(notifyAlert);
             notifyAlert = null;
         }
