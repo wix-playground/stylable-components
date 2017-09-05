5b04e0d40f3a1ce1901f622f6b4f7328d4b6931e
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-05 13:30:42 +0300

    after review

--- src/components/screen-reader-notification/screen-reader-notification.tsx
+++ src/components/screen-reader-notification/screen-reader-notification.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
-import {Stylesheet} from 'stylable';
 import {SBStateless} from 'stylable-react-component';
 import styles from './screen-reader-notification.st.css';
 
-export type Stylesheet = Stylesheet;
 export const ScreenReaderNotification = SBStateless(
     ({children}: {children: React.ReactNode}) => {
         return (
