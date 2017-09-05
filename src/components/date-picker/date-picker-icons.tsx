b03ddef8bf682c67e39d59ce61c7a7bae4f4c565
Kieran Williams <kieranw@wix.com>
2017-09-05 11:52:19 +0300

    Removed styling from element

--- src/components/date-picker/date-picker-icons.tsx
+++ src/components/date-picker/date-picker-icons.tsx
@@ -6,10 +6,6 @@ export const CalendarIcon: React.SFC<React.HTMLAttributes<SVGElement>> = props =
              xmlns="http://www.w3.org/2000/svg"
              focusable="false"
              viewBox="0 0 24 24"
-             stroke="currentColor"
-             strokeWidth="2"
-             strokeLinecap="round"
-             strokeLinejoin="round"
              {...props}
         >
             <polyline points="6 9 12 15 18 9" />
