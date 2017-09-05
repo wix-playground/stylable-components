f647e0fedbc8b99bb08a0ccb7a20ae734cba5a59
Kieran Williams <kieranw@wix.com>
2017-09-05 11:19:59 +0300

    Made changes re: PR

--- src/components/date-picker/date-picker-icons.tsx
+++ src/components/date-picker/date-picker-icons.tsx
@@ -4,10 +4,8 @@ export const CalendarIcon: React.SFC<React.HTMLAttributes<SVGElement>> = props =
     return (
         <svg
              xmlns="http://www.w3.org/2000/svg"
-             width="24"
-             height="24"
+             focusable="false"
              viewBox="0 0 24 24"
-             fill="none"
              stroke="currentColor"
              strokeWidth="2"
              strokeLinecap="round"
