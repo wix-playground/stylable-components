88bab9b9c0bb8538bfb850af734620e9a3624634
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-05 16:58:29 +0300

    added typings

--- src/components/screen-reader-notification/screen-reader-notification.tsx
+++ src/components/screen-reader-notification/screen-reader-notification.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {SBStateless} from 'stylable-react-component';
 import styles from './screen-reader-notification.st.css';
 
-export const ScreenReaderNotification = SBStateless(
+export const ScreenReaderNotification: React.SFC = SBStateless(
     ({children}: {children: React.ReactNode}) => {
         return (
             <p
