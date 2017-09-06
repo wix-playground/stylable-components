25e165d2bb06b38884b49cd9f66d68fe322c614e
Volodymyr Kobeliatsky <volodymyrk@wix.com>
2017-09-06 15:09:00 +0300

    number-input demo improvements;

--- demo/components/number-input.demo.tsx
+++ demo/components/number-input.demo.tsx
@@ -26,11 +26,11 @@ export class NumberInputDemo extends React.Component<{}, State> {
                         value={basicValue}
                         step={1}
                         max={100}
-                        onChange={this.handleSmallValueChange}
+                        onChange={this.handleBasicValueChange}
                         placeholder="How Many?"
                     >
-                        <span data-slot="prefix">$</span>
-                        <span data-slot="suffix">{this.commentIcon(basicValue)}</span>
+                        <TrendingUp data-slot="prefix" />
+                        <span data-slot="suffix">USD</span>
                     </NumberInput>
                 </div>
                 <div>
@@ -40,7 +40,7 @@ export class NumberInputDemo extends React.Component<{}, State> {
                         step={2}
                         min={-5}
                         max={5}
-                        onChange={this.handleValueChange}
+                        onChange={this.handleSharedValueChange}
                         placeholder="How Many?"
                     />
                 </div>
@@ -57,7 +57,7 @@ export class NumberInputDemo extends React.Component<{}, State> {
                     <NumberInput
                         error
                         value={sharedValue}
-                        onChange={this.handleValueChange}
+                        onChange={this.handleSharedValueChange}
                         placeholder="Always wrong!"
                     />
                 </div>
@@ -75,21 +75,12 @@ export class NumberInputDemo extends React.Component<{}, State> {
         );
     }
 
-    private handleValueChange = (value?: number) => this.setState({sharedValue: value});
-    private handleSmallValueChange = (value?: number) => this.setState({basicValue: value});
-    private commentIcon = (value?: number) => (
-        Number(value) > 0 ?
-        trendingUp({style: {verticalAlign: 'middle'}}) :
-        Number(value) < 0 ?
-            trendingDown({style: {verticalAlign: 'middle'}}) :
-            Number(value) === undefined ?
-                loader({style: {verticalAlign: 'middle'}}) :
-                minus({style: {verticalAlign: 'middle'}})
-    )
+    private handleSharedValueChange = (value?: number) => this.setState({sharedValue: value});
+    private handleBasicValueChange = (value?: number) => this.setState({basicValue: value});
 
 }
 
-const trendingUp = (props?: React.SVGAttributes<SVGElement>) => (
+const TrendingUp = (props?: React.SVGAttributes<SVGElement>) => (
     <svg
         {...props}
         xmlns="http://www.w3.org/2000/svg"
@@ -104,64 +95,5 @@ const trendingUp = (props?: React.SVGAttributes<SVGElement>) => (
     >
         <polyline points="23 6 13.5 15.5 8.5 10.5 1 18"/>
         <polyline points="17 6 23 6 23 12"/>
-    </svg>
-);
-
-const trendingDown = (props?: React.SVGAttributes<SVGElement>) => (
-    <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-    >
-        <polyline points="23 18 13.5 8.5 8.5 13.5 1 6"/>
-        <polyline points="17 18 23 18 23 12"/>
-    </svg>
-);
-
-const loader = (props?: React.SVGAttributes<SVGElement>) => (
-    <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-    >
-        <line x1="12" y1="2" x2="12" y2="6"/>
-        <line x1="12" y1="18" x2="12" y2="22"/>
-        <line x1="4.93" y1="4.93" x2="7.76" y2="7.76"/>
-        <line x1="16.24" y1="16.24" x2="19.07" y2="19.07"/>
-        <line x1="2" y1="12" x2="6" y2="12"/>
-        <line x1="18" y1="12" x2="22" y2="12"/>
-        <line x1="4.93" y1="19.07" x2="7.76" y2="16.24"/>
-        <line x1="16.24" y1="7.76" x2="19.07" y2="4.93"/>
-    </svg>
-);
-
-const minus = (props?: React.SVGAttributes<SVGElement>) => (
-    <svg
-        {...props}
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-    >
-        <line x1="5" y1="12" x2="19" y2="12"/>
     </svg>
 );
