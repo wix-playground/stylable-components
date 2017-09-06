f493a9c8b06e8545b5c07bccab7803ab9fc9ac59
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-06 10:37:11 +0300

    added simple test

--- src/components/screen-reader-notification/screen-reader-notification.tsx
+++ src/components/screen-reader-notification/screen-reader-notification.tsx
@@ -6,6 +6,7 @@ export const ScreenReaderNotification: React.SFC = SBStateless(
     ({children}: {children: React.ReactNode}) => {
         return (
             <p
+                data-automation-id="SCREEN_READER_NOTIFICATION"
                 role="alert"
                 children={children}
             />
