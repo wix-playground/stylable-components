795dd11b2382c73053954813d560235442262718
Kieran Williams <kieranw@wix.com>
2017-09-06 15:48:23 +0300

    Added svg icon from wix-style-react

--- src/components/date-picker/date-picker-icons.tsx
+++ src/components/date-picker/date-picker-icons.tsx
@@ -5,19 +5,32 @@ export const CalendarIcon: React.SFC<React.HTMLAttributes<SVGElement>> = props =
         <svg
             xmlns="http://www.w3.org/2000/svg"
             focusable="false"
-            viewBox="0 0 24 24"
+            viewBox="0 0 16 16"
             {...props}
         >
-            <path
-                d={`M24,40 L24,41 L16,41 L16,40 L15,40` +
-                `L15,41 L13,41 L13,42 L15,42 L15,43 L16,43` +
-                `L16,42 L24,42 L24,43 L25,43 L25,42 L27,42` +
-                `L27,45 L13,45 L13,42 L12,42 L12,54.9999375` +
-                `L13,54.9999375 L13,46 L27,46 L27,54.9999375` +
-                `L28,54.9999375 L28,42 L27.0000625,42 L27.000` +
-                `0625,41 L25,41 L25,40 L24,40 Z M13,56 L27.00` +
-                `00625,56 L27.0000625,55 L13,55 L13,56 Z`}
-            />
+            <g id="Version-B" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g id="Pickup-hours---add-execption-v3" transform="translate(-459.000000, -443.000000)" fill="#3899EC">
+                    <g id="All" transform="translate(183.000000, 189.000000)">
+                        <g id="Modal">
+                            <g id="Form-Copy-2" transform="translate(264.000000, 88.000000)">
+                                <g id="Date" transform="translate(0.000000, 126.000000)">
+                                    <path
+                                        d={`M24,40 L24,41 L16,41 L16,40 L15,40` +
+                                        `L15,41 L13,41 L13,42 L15,42 L15,43 L16,43` +
+                                        `L16,42 L24,42 L24,43 L25,43 L25,42 L27,42` +
+                                        `L27,45 L13,45 L13,42 L12,42 L12,54.9999375` +
+                                        `L13,54.9999375 L13,46 L27,46 L27,54.9999375` +
+                                        `L28,54.9999375 L28,42 L27.0000625,42 L27.000` +
+                                        `0625,41 L25,41 L25,40 L24,40 Z M13,56 L27.00` +
+                                        `00625,56 L27.0000625,55 L13,55 L13,56 Z`}
+                                        id="Icon"
+                                    />
+                                </g>
+                            </g>
+                        </g>
+                    </g>
+                </g>
+            </g>
         </svg>
     );
 };
