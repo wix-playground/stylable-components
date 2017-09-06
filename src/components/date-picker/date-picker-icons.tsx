086f6ea9174889cf34c92a68d2fbd715184f0acf
Kieran Williams <kieranw@wix.com>
2017-09-06 15:49:01 +0300

    Removed ids

--- src/components/date-picker/date-picker-icons.tsx
+++ src/components/date-picker/date-picker-icons.tsx
@@ -8,12 +8,12 @@ export const CalendarIcon: React.SFC<React.HTMLAttributes<SVGElement>> = props =
             viewBox="0 0 16 16"
             {...props}
         >
-            <g id="Version-B" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                <g id="Pickup-hours---add-execption-v3" transform="translate(-459.000000, -443.000000)" fill="#3899EC">
-                    <g id="All" transform="translate(183.000000, 189.000000)">
-                        <g id="Modal">
-                            <g id="Form-Copy-2" transform="translate(264.000000, 88.000000)">
-                                <g id="Date" transform="translate(0.000000, 126.000000)">
+            <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                <g transform="translate(-459.000000, -443.000000)" fill="#3899EC">
+                    <g transform="translate(183.000000, 189.000000)">
+                        <g>
+                            <g transform="translate(264.000000, 88.000000)">
+                                <g transform="translate(0.000000, 126.000000)">
                                     <path
                                         d={`M24,40 L24,41 L16,41 L16,40 L15,40` +
                                         `L15,41 L13,41 L13,42 L15,42 L15,43 L16,43` +
@@ -23,7 +23,6 @@ export const CalendarIcon: React.SFC<React.HTMLAttributes<SVGElement>> = props =
                                         `L28,54.9999375 L28,42 L27.0000625,42 L27.000` +
                                         `0625,41 L25,41 L25,40 L24,40 Z M13,56 L27.00` +
                                         `00625,56 L27.0000625,55 L13,55 L13,56 Z`}
-                                        id="Icon"
                                     />
                                 </g>
                             </g>
