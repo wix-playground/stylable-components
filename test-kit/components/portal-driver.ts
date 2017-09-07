1145e6208282c01313a4646b28f71a04e0d357bd
Lior Zisman <liorzi@wix.com>
2017-09-07 14:43:36 +0300

    portal driver

---
+++ test-kit/components/portal-driver.ts
@@ -0,0 +1,8 @@
+import {DriverBase, simulate} from 'test-drive-react';
+import {Portal} from '../../src';
+
+export class PortalTestDriver extends DriverBase {
+    public static ComponentClass = Portal;
+
+
+}
