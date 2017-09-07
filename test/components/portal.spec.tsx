07b2482737b96dabce91693b304d53ce1ef1004a
Lior Zisman <liorzi@wix.com>
2017-09-07 16:36:32 +0300

    portalz completed

--- test/components/portal.spec.tsx
+++ test/components/portal.spec.tsx
@@ -27,13 +27,15 @@ describe('<Portal />', function() {
     });
 
     it('applies supplied styles to the popup and updates them if changed', async function() {
-        const {container} = clientRenderer.render(
+        const {container, result} = clientRenderer.render(
             <Portal style={{position: 'absolute'}}>
                 <span data-automation-id="SPAN">Portal Body</span>
             </Portal>
         );
 
-        await waitFor(() => expect(bodySelect(portalId)).to.have.nested.property('style.position', 'absolute'));
+        const driver = new PortalTestDriver(result as Portal);
+
+        await waitFor(() => expect(driver.portal).to.have.nested.property('style.position', 'absolute'));
 
         clientRenderer.render(
             <Portal style={{position: 'fixed'}}>
@@ -42,35 +44,39 @@ describe('<Portal />', function() {
             container
         );
 
-        await waitFor(() => expect(bodySelect(portalId)).to.have.nested.property('style.position', 'fixed'));
+        await waitFor(() => expect(driver.portal).to.have.nested.property('style.position', 'fixed'));
     });
 
     it('removes the component when unmounting', async function() {
         const container = document.body.appendChild(document.createElement('div'));
-        clientRenderer.render(
+        const {result} = clientRenderer.render(
             <Portal>
                 <span data-automation-id="SPAN">Popup Body</span>
             </Portal>, container);
 
-        await waitFor(() => expect(bodySelect(portalId)).to.be.present());
+        const driver = new PortalTestDriver(result as Portal);
+
+        await waitFor(() => expect(driver.portal).to.be.present());
 
         ReactDOM.unmountComponentAtNode(container);
         document.body.removeChild(container);
 
         await waitFor(() => {
-            expect(bodySelect(portalId)).to.be.absent();
-            expect(bodySelect('SPAN')).to.be.absent();
+            expect(driver.portal).to.be.absent();
+            expect(driver.content).to.be.absent();
         });
     });
 
     it('updates the portal content if the children are changed', async function() {
-        const {container} = clientRenderer.render(
+        const {container, result} = clientRenderer.render(
             <Portal>
                 <span data-automation-id="SPAN">Portal Body</span>
             </Portal>
         );
 
-        await waitFor(() => expect(bodySelect('SPAN')).to.be.present());
+        const driver = new PortalTestDriver(result as Portal);
+
+        await waitFor(() => expect(driver.content[0]).to.be.present());
 
         clientRenderer.render(
             <Portal>
@@ -79,19 +85,21 @@ describe('<Portal />', function() {
             container
         );
 
-        await waitFor(() => expect(bodySelect('UPDATED_SPAN')).to.be.present());
+        await waitFor(() => expect(driver.content[0]).to.be.present());
     });
 
     it('renders the portal in the bottom of the DOM', async function() {
-        const {container} = clientRenderer.render(
+        const {container, result} = clientRenderer.render(
             <Portal>
                 <span data-automation-id="SPAN">Portal Body</span>
             </Portal>
         );
 
+        const driver = new PortalTestDriver(result as Portal);
+
         await waitFor(() => {
-            const portal = bodySelect<HTMLElement>(portalId)!;
-            const children = bodySelect<HTMLElement>('SPAN')!;
+            const portal = driver.portal!;
+            const children = driver.content[0];
 
             /* tslint:disable:no-bitwise */
             expect(portal.compareDocumentPosition(children) & Node.DOCUMENT_POSITION_CONTAINED_BY,
@@ -103,12 +111,14 @@ describe('<Portal />', function() {
     });
 
     it('renders with a className passed as a prop', async function() {
-        clientRenderer.render(
+        const {result} = clientRenderer.render(
             <Portal className="test-class">
                 <span data-automation-id="SPAN">Portal Body</span>
             </Portal>
         );
 
-        await waitFor(() => expect(bodySelect(portalId)!.className).to.contain('test-class'));
+        const driver = new PortalTestDriver(result as Portal);
+
+        await waitFor(() => expect((driver.portal as Element).className).to.contain('test-class'));
     });
 });
