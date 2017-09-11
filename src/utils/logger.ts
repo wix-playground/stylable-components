813c29dedbc5d7d0d62861522ba6eb07b4b9b203
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-11 17:56:24 +0300

    covered "once" functions

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,7 @@
 import {getGlobalConfig} from 'wix-react-tools';
-
 export type Log = (condition: any, message: string, ...args: any[]) => void;
 type Method = 'log' | 'warn' | 'error';
 
-const map = new Map();
 const createLogFunction = (method: Method): Log => (condition, message, ...args) => {
     if (condition || !getGlobalConfig().devMode) {
         return;
@@ -11,16 +9,24 @@ const createLogFunction = (method: Method): Log => (condition, message, ...args)
     const formated = args.reduce((str, item) => str.replace(/%s/, item), message);
     console[method](new Error(formated));
 };
-const once = (fn: Log): Log => (condition, message, ...args) => {
+const once = (fn: Log, map: Map<string, boolean>): Log => (condition, message, ...args) => {
     if (!condition && !map.has(message)) {
         map.set(message, true);
         fn(condition, message, ...args);
     }
 };
 
-export const warn = createLogFunction('warn');
-export const error = createLogFunction('error');
-export const log = createLogFunction('log');
-export const warnOnce = once(warn);
-export const errorOnce = once(error);
-export const logOnce = once(log);
+export const createLogger = () => {
+    const map = new Map();
+    const result: {
+        [key: string]: Log
+    } = {};
+    return ['log', 'warn', 'error'].reduce((obj, method: Method) => {
+        const fn = createLogFunction(method);
+        obj[method] = fn;
+        obj[method + 'Once'] = once(fn, map);
+        return obj;
+    }, result);
+};
+
+export const {log, warn, error, logOnce, warnOnce, errorOnce} = createLogger();
--- test/components-utils/logger.spec.ts
+++ test/components-utils/logger.spec.ts
@@ -1,102 +1,114 @@
-import {setGlobalConfig, overrideGlobalConfig} from 'wix-react-tools';
 import {expect, sinon} from 'test-drive-react';
-import * as logger from '../../src/utils/logger';
+import {overrideGlobalConfig, setGlobalConfig} from 'wix-react-tools';
+import {createLogger} from '../../src/utils/logger';
 
 function assetError(fn: sinon.SinonSpy, err: Error) {
-	expect(fn).to.calledOnce;
-	expect(fn.firstCall.args[0]).instanceof(Error);
-	expect(fn.firstCall.args[0]).property('message', err.message);
+    expect(fn).to.calledOnce;
+    expect(fn.firstCall.args[0]).instanceof(Error);
+    expect(fn.firstCall.args[0]).property('message', err.message);
 }
 
 type Method = 'log' | 'warn' | 'error';
 const methods: Method[] = ['log', 'warn', 'error'];
 
 describe.only('logger', () => {
-	let spies: {
-		[key: string]: sinon.SinonSpy
-	} = {};
-	beforeEach(() => {
-		methods.forEach(method => {
-			const fn = sinon.spy();
-			spies[method] = fn;
-			sinon.stub(console, method).callsFake(fn);
-		});
-	});
+    let spies: {
+        [key: string]: sinon.SinonSpy
+    } = {};
+    let logger: any;
+    beforeEach(() => {
+        methods.forEach(method => {
+            const fn = sinon.spy();
+            spies[method] = fn;
+            sinon.stub(console, method).callsFake(fn);
+            logger = createLogger();
+        });
+    });
 
-	afterEach(() => {
-		methods.forEach(method => {
-			(console[method] as sinon.SinonExpectation).restore();
-		});
-		spies = {};
-	});
+    afterEach(() => {
+        methods.forEach(method => {
+            (console[method] as sinon.SinonExpectation).restore();
+        });
+        spies = {};
+    });
 
-	describe('setGlobalConfig({devMode: true})', () => {
-		beforeEach(() => {
-			setGlobalConfig({devMode: true});
-		});
-		afterEach(() => {
-			overrideGlobalConfig({});
-		});
-		methods.forEach(method => {
-			describe(method, () => {
-				it('trully condition', () => {
-					logger[method](true, 'Warning message')
-					expect(spies[method]).to.not.be.called;
-				});
-				it('no formating', () => {
-					logger[method](false, 'Warning message')
-					assetError(spies[method], new Error('Warning message'));
-				});
-				it('formaring', () => {
-					logger[method](false, 'Warning message, a=%s, b=%s', 11, 12)
-					assetError(spies[method], new Error('Warning message, a=11, b=12'));
-				});
-			})
-		})
-	});
+    describe('setGlobalConfig({devMode: true})', () => {
+        beforeEach(() => {
+            setGlobalConfig({devMode: true});
+        });
+        afterEach(() => {
+            overrideGlobalConfig({});
+        });
+        methods.forEach(method => {
+            describe(method, () => {
+                it('trully condition', () => {
+                    logger[method](true, 'Warning message');
+                    expect(spies[method]).to.not.be.called;
+                });
+                it('no formating', () => {
+                    logger[method](false, 'Warning message');
+                    assetError(spies[method], new Error('Warning message'));
+                });
+                it('formaring', () => {
+                    logger[method](false, 'Warning message, a=%s, b=%s', 11, 12);
+                    assetError(spies[method], new Error('Warning message, a=11, b=12'));
+                });
+                it('call x 2', () => {
+                    logger[method](false, 'Warning message');
+                    logger[method](false, 'Warning message');
+                    expect(spies[method]).to.calledTwice;
+                });
+                it('call once x 2', () => {
+                    logger[method + 'Once'](false, 'Warning message');
+                    logger[method + 'Once'](false, 'Warning message');
+                    expect(spies[method]).to.calledOnce;
+                });
+            });
+        });
+    });
 
-	describe('setGlobalConfig({devMode: false})', () => {
-		beforeEach(() => {
-			setGlobalConfig({devMode: false});
-		});
-		afterEach(() => {
-			overrideGlobalConfig({});
-		});
-		methods.forEach(method => {
-			describe(method, () => {
-				it('trully condition', () => {
-					logger[method](true, 'Warning message')
-					expect(spies[method]).to.not.be.called;
-				});
-				it('no formating', () => {
-					logger[method](false, 'Warning message')
-					expect(spies[method]).to.not.be.called;
-				});
-				it('formaring', () => {
-					logger[method](false, 'Warning message, a=%s, b=%s', 11, 12)
-					expect(spies[method]).to.not.be.called;
-				});
-			})
-		})
-	});
+    describe('setGlobalConfig({devMode: false})', () => {
+        beforeEach(() => {
+            setGlobalConfig({devMode: false});
+        });
+        afterEach(() => {
+            overrideGlobalConfig({});
+        });
+        methods.forEach(method => {
+            describe(method, () => {
+                it('trully condition', () => {
+                    logger[method](true, 'Warning message');
+                    expect(spies[method]).to.not.be.called;
+                });
+                it('no formating', () => {
+                    logger[method](false, 'Warning message');
+                    expect(spies[method]).to.not.be.called;
+                });
+                it('formaring', () => {
+                    logger[method](false, 'Warning message, a=%s, b=%s', 11, 12);
+                    expect(spies[method]).to.not.be.called;
+                });
+            });
+        });
+    });
 
-	describe('no setGlobalConfig', () => {
-		methods.forEach(method => {
-			describe(method, () => {
-				it('trully condition', () => {
-					logger[method](true, 'Warning message')
-					expect(spies[method]).to.not.be.called;
-				});
-				it('no formating', () => {
-					logger[method](false, 'Warning message')
-					expect(spies[method]).to.not.be.called;
-				});
-				it('formaring', () => {
-					logger[method](false, 'Warning message, a=%s, b=%s', 11, 12)
-					expect(spies[method]).to.not.be.called;
-				});
-			})
-		})
-	});
+    describe('no setGlobalConfig', () => {
+        methods.forEach(method => {
+            describe(method, () => {
+                it('trully condition', () => {
+                    logger[method](true, 'Warning message');
+                    expect(spies[method]).to.not.be.called;
+                });
+                it('no formating', () => {
+                    logger[method](false, 'Warning message');
+                    expect(spies[method]).to.not.be.called;
+                });
+                it('formaring', () => {
+                    logger[method](false, 'Warning message, a=%s, b=%s', 11, 12);
+                    expect(spies[method]).to.not.be.called;
+                });
+            });
+        });
+    });
 
 });
