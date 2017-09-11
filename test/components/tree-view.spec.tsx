386c87353d3cbb72c572fb9e65daa8fb6dca4d1f
Kieran Williams <kieranw@wix.com>
2017-09-11 10:04:46 +0300

    Copied back a few methods teporarily

--- test/components/tree-view.spec.tsx
+++ test/components/tree-view.spec.tsx
@@ -1,3 +1,4 @@
+import {observable} from 'mobx';
 import * as React from 'react';
 import {ClientRenderer, expect, sinon, waitFor} from 'test-drive-react';
 import {TreeView} from '../../src';
@@ -32,6 +33,50 @@ const treeData: TreeItemData[] = [
         ]
     }
 ];
+
+// duplicating the data so i can pass a new object to the non-mobx version
+const newTreeData = JSON.parse(JSON.stringify(treeData));
+newTreeData[0].children![2].children!.push({label: 'Kaiserschmarrn'});
+
+export interface TreeViewWrapperState {
+    treeData: object[];
+}
+
+export class TreeViewWrapper extends React.Component<{}, TreeViewWrapperState> {
+    public state = {treeData};
+
+    public render() {
+        return <TreeView dataSource={this.state.treeData}/>;
+    }
+
+    public switchDataSource = () => {
+        this.setState({
+            treeData: newTreeData
+        });
+    }
+}
+
+export class TreeViewMobxWrapper extends React.Component<{}, {}> {
+    @observable private obsTreeData: TreeItemData[] = treeData;
+
+    public render() {
+        return <TreeView dataSource={this.obsTreeData}/>;
+    }
+
+    public modifyMobxDataSource = () => {
+        this.obsTreeData[0].children![2].children!.push({label: 'Kaiserschmarrn'});
+    }
+}
+
+function getLabelsList(data: { label: string, children?: object[] }): string[] {
+    return [data.label]
+        .concat(...(data.children || [])
+            .map(getLabelsList));
+}
+
+function getAllNodeLabels(data: object[]): string[] {
+    return data.map(getLabelsList).reduce((prev, next) => [...prev, ...next]);
+}
 
 describe('<TreeView />', () => {
     const clientRenderer = new ClientRenderer();
