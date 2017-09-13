a5ecdfaf9e6537eef58f4ebc3defa7869b61e1a9
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-13 11:42:01 +0300

    ContextProvider - tests

---
+++ src/components/context-provider/context-provider.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class ContextProvider extends React.Component {
+    render() {
+        return null;
+    }
+}
---
+++ test/components/context-provider.spec.tsx
@@ -0,0 +1,153 @@
+import * as React from 'react';
+import * as PropTypes from 'prop-types';
+import {findDOMNode} from 'react-dom';
+import {ClientRenderer, expect, sinon, waitFor} from 'test-drive-react';
+import {ContextProvider} from '../../src';
+
+describe.only('<ContextProvider/>', () => {
+    const clientRenderer = new ClientRenderer();
+    afterEach(() => clientRenderer.cleanup());
+
+    describe('render with no properties', () => {
+        let root: any;
+        beforeEach(() => {
+            const renderer = clientRenderer.render(<ContextProvider/>);
+            root = findDOMNode(renderer.result as React.ReactInstance);
+        });
+        it('should render div', () => {
+            expect(root).instanceof(HTMLDivElement);
+        });
+    })
+
+    describe('render with tagName="p" ', () => {
+        let root: any;
+        beforeEach(() => {
+            const renderer = clientRenderer.render(<ContextProvider tagName="p"/>);
+            root = findDOMNode(renderer.result as React.ReactInstance);
+        });
+        it('should render paragraph', () => {
+            expect(root).instanceof(HTMLParagraphElement);
+        });
+    });
+
+    describe('render with children ', () => {
+        let renderer: any;
+        let root: any;
+        beforeEach(() => {
+            renderer = clientRenderer.render(
+                <ContextProvider>
+                    <div data-automation-id="TEST_DIV">Hello</div>
+                </ContextProvider>
+            );
+            root = findDOMNode(renderer.result as React.ReactInstance);
+        });
+        it('should render child', () => {
+            expect(renderer.select('TEST_DIV')).to.not.null;
+        });
+    });
+
+    describe('render with "style" and "className" ', () => {
+        let renderer: any;
+        let root: any;
+        beforeEach(() => {
+            renderer = clientRenderer.render(
+                <ContextProvider
+                    className='context-div'
+                    style={{width: 10}}
+                />
+            );
+            root = findDOMNode(renderer.result as React.ReactInstance);
+        });
+        it('should have "className"', () => {
+            expect(root).to.have.class('context-div');
+        });
+        it('should have inline styles', () => {
+            expect(root).attr('style', 'width:10px');
+        });
+    });
+
+    describe('render with dir="rtl" ', () => {
+        class Inner extends React.Component {
+            static contextTypes = {
+                'context-provider-dir': PropTypes.string
+            }
+            render() {
+                return <div data-automation-id="TEST_DIV" dir={this.context['context-provider-dir']}/>;
+            }
+        }
+        let renderer: any;
+        let root: any;
+        beforeEach(() => {
+            renderer = clientRenderer.render(
+                <ContextProvider dir="rtl">
+                    <Inner/>
+                </ContextProvider>
+            );
+            root = findDOMNode(renderer.result as React.ReactInstance);
+        });
+        it('should render component with div="rtl"', () => {
+            expect(root).attr('dir', 'rtl');
+        });
+        it('should pass "dir" as "context-provider-dir" context property', () => {
+            expect(renderer.select('TEST_DIV')).attr('dir', 'rtl');
+        });
+    });
+
+    describe('render with context-foo="bar"', () => {
+        class Inner extends React.Component {
+            static contextTypes = {
+                foo: PropTypes.string
+            }
+            render() {
+                return <div data-automation-id="TEST_DIV">{this.context.foo}</div>
+            }
+        }
+
+        let renderer: any;
+        let root: any;
+
+        beforeEach(() => {
+            renderer = clientRenderer.render(
+                <ContextProvider context-foo="bar">
+                    <Inner/>
+                </ContextProvider>
+            );
+            root = findDOMNode(renderer.result as React.ReactInstance);
+        });
+
+        it('should pass the contenxt {foo: "bar"}', () => {
+            expect(renderer.select('TEST_DIV')).text('bar');
+        });
+
+    });
+
+    describe('render with Symbol property', () => {
+        const prop = Symbol();
+
+        class Inner extends React.Component {
+            static contextTypes = {
+                [props]: PropTypes.string
+            }
+            render() {
+                return <div data-automation-id="TEST_DIV">{this.context[prop]}</div>
+            }
+        }
+
+        let renderer: any;
+        let root: any;
+
+        beforeEach(() => {
+            renderer = clientRenderer.render(
+                <ContextProvider {...{[prop]: 'baz'}}>
+                    <Inner/>
+                </ContextProvider>
+            );
+            root = findDOMNode(renderer.result as React.ReactInstance);
+        });
+
+        it('should pass the contenxt {[Symbol]: "baz"}', () => {
+            expect(renderer.select('TEST_DIV')).text('baz');
+        });
+
+    });
+});
