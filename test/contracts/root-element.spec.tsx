97dc08996611e4cdff6bff88f2ef88fa2a38c79c
Evgen Filatov <evgen.filatov@gmail.com>
2017-09-14 12:03:48 +0300

    added context provider in failingComponents of root contract

--- test/contracts/root-element.spec.tsx
+++ test/contracts/root-element.spec.tsx
@@ -7,7 +7,7 @@ import {isReactComponent} from '../utils/is-react-component';
 const allComponents = Object.keys(WixReactComponents);
 const failingComponents = [
     'TreeView', 'NumberInput', 'Toggle', 'BirthdayPicker',
-     'Portal', 'Popup', 'TimePicker', 'Modal'
+     'Portal', 'Popup', 'TimePicker', 'Modal', 'ContextProvider'
 ];
 
 describe('Root Element contract', function() {
