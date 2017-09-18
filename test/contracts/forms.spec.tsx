62bff9cc3e7ce9b349e2173780065f817a35faad
Jiri Tobisek <jiri@wix.com>
2017-09-18 14:57:15 +0300

    WIP

--- test/contracts/forms.spec.tsx
+++ test/contracts/forms.spec.tsx
@@ -1,71 +1,109 @@
 import React = require('react');
-import {ClientRenderer, expect} from 'test-drive-react';
-import {CheckBox, DropDown, DatePicker, SelectionList} from '../../src';
+import {ClientRenderer, expect, waitFor} from 'test-drive-react';
+import {CheckBox, DropDown, DatePicker, SelectionList, NumberInput, Slider, Toggle} from '../../src';
+import {sleep} from "../utils/sleep";
+import {func} from "prop-types";
+import {SyntheticEvent} from "react";
 
 describe('Form contract of', function() {
 
+    const testName = 'testInput';
     const clientRenderer = new ClientRenderer();
 
     afterEach(function() {
         clientRenderer.cleanup();
     });
 
-    async function testFormContract(componentElement: React.ReactElement<any>, testValue: any,
-                                    expectedValue: string = testValue) {
-        const testName = 'testInput';
-        let formElement: HTMLFormElement;
+    async function testFormContract(componentElement: React.ReactElement<any>, testValue: any, expectedQuery: string) {
+
+        let loaderLocation: Location;
+
+        function onLoad(event: SyntheticEvent<HTMLIFrameElement>) {
+            const iframe = event.target as HTMLIFrameElement;
+            loaderLocation = iframe.contentWindow.location;
+        }
+
+        function submitForm(form: HTMLFormElement): void {
+            form && form.submit();
+        }
+
         const testedElement = React.cloneElement(componentElement, {
             name: testName,
             value: testValue
         });
         const form = (
-            <form ref={element => formElement = element as HTMLFormElement}>
-                {testedElement}
-            </form>
+            <div>
+                <form
+                    ref={(element: HTMLFormElement) => submitForm(element)}
+                    target="formLoader"
+                    method="get"
+                    action="about:config"
+                >
+                    {testedElement}
+                </form>
+                <iframe name="formLoader" onLoad={onLoad}/>
+            </div>
         );
-        const {waitForDom} = clientRenderer.render(form);
-        await waitForDom(() => {
-            const formInput = formElement![testName] as HTMLInputElement;
-            expect(formInput).to.be.an.instanceOf(HTMLInputElement);
-            expect(formInput.value).to.equal(expectedValue);
+        clientRenderer.render(form);
+        await waitFor(() => {
+            expect(loaderLocation.search).to.equal(expectedQuery);
         });
     }
 
-    it('CheckBox', async function() {
-        await testFormContract(<CheckBox />, true, 'on');
-        await testFormContract(<CheckBox formValue="custom"/>, true, 'custom');
+    it('CheckBox (checked)', async function() {
+        await testFormContract(<CheckBox />, true, `?${testName}=on`);
+    });
+
+    it('CheckBox (unchecked)', async function () {
+        await testFormContract(<CheckBox />, false, '');
+    });
+
+    it('CheckBox (custom value)', async function () {
+        await testFormContract(<CheckBox formValue="custom"/>, true, `?${testName}=custom`);
     });
 
     it('DatePicker', async function() {
         const sampleDate = '2017-02-01';
-        await testFormContract(<DatePicker/>, new Date(sampleDate), sampleDate);
+        await testFormContract(<DatePicker/>, new Date(sampleDate), `?${testName}=${sampleDate}`);
     });
 
     it('DropDown', async function() {
-        await testFormContract(<DropDown dataSource={['A', 'B', 'C']} />, 'B');
+        await testFormContract(<DropDown dataSource={['A', 'B', 'C']} />, 'B', `?${testName}=B`);
     });
 
-    // NumberInput
+    it('NumberInput', async function() {
+        await testFormContract(<NumberInput />, 666, `?${testName}=666`);
+    });
 
     // RadioGroup - wait for Marton
 
-    it('SelectionList', async function() {
-        await testFormContract(<SelectionList dataSource={['A', 'B', 'C']} />, 'B');
+    it('SelectionList (dataSource)', async function() {
+        await testFormContract(<SelectionList dataSource={['A', 'B', 'C']} />, 'B', `?${testName}=B`);
+
+    });
+
+    it('SelectionList (children)', async function () {
         await testFormContract(
             <SelectionList>
                 <div data-value="A">Adalbert</div>
                 <div data-value="B">Bombino</div>
                 <div data-value="C">Cicciolina</div>
             </SelectionList>
-        , 'B');
+            , 'B', `?${testName}=B`);
     });
 
-    // Slider
+    it('Slider', async function() {
+        await testFormContract(<Slider />, 666, `?${testName}=666`);
+    });
 
-    // Stepper
+    it('Toggle', async function() {
+        await testFormContract(<Toggle />, true, `?${testName}=on`);
+    });
+
+    it('Toggle (unchecked)', async function () {
+        await testFormContract(<Toggle />, false, '');
+    });
 
     // TimePicker
-
-    // Toggle
 
 });
