aff22fa6f232973ef68741f5df4c4ec4c687e405
Volodymyr Kobeliatsky <vladimir.kobeliatsky@gmail.com>
2017-09-20 12:30:45 +0300

    Added test driver for NumberInput (#356)

--- test-kit/components/index.ts
+++ test-kit/components/index.ts
@@ -5,3 +5,5 @@ export * from './portal-driver';
 export * from './radio-group-driver';
 export * from './selection-list-driver';
 export * from './date-picker-driver';
+export * from './number-input-driver';
+export * from './stateful-uncontrolled-number-input-driver';
