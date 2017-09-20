69ea6a4d9850e8b2f92fe5db0335f8de78ddaa9a
Volodymyr Kobeliatsky <vladimir.kobeliatsky@gmail.com>
2017-09-20 12:30:45 +0300

    Added test driver for NumberInput (#356)

---
+++ test-kit/components/number-input-driver.ts
@@ -0,0 +1,65 @@
+import {codes as KeyCodes} from 'keycode';
+import * as React from 'react';
+import {DriverBase, simulate} from 'test-drive-react';
+import {NumberInput} from '../../src';
+import {Modifiers} from '../../src/components/stepper';
+import {simulateKeyInput} from '../../test/utils';
+
+export class NumberInputDriver extends DriverBase {
+    public static ComponentClass = NumberInput;
+    public get nativeInput() {
+        return this.select('NATIVE_INPUT_NUMBER');
+    }
+
+    public get stepper() {
+        return this.select('NUMBER_INPUT_STEPPER');
+    }
+
+    public get increment() {
+        return this.select('STEPPER_INCREMENT');
+    }
+
+    public get decrement() {
+        return this.select('STEPPER_DECREMENT');
+    }
+
+    public get prefix() {
+        return this.select('PREFIX');
+    }
+
+    public get suffix() {
+        return this.select('SUFFIX');
+    }
+
+    public clickIncrement(opts?: Modifiers) {
+        simulate.click(this.increment, opts);
+    }
+
+    public clickDecrement(opts?: Modifiers) {
+        simulate.click(this.decrement, opts);
+    }
+
+    public pressUpKey(opts?: Modifiers) {
+        simulate.keyDown(this.nativeInput, {keyCode: KeyCodes.up, ...opts});
+    }
+
+    public pressDownKey(opts?: Modifiers) {
+        simulate.keyDown(this.nativeInput, {keyCode: KeyCodes.down, ...opts});
+    }
+
+    public pressEnter() {
+        simulate.keyDown(this.nativeInput, {keyCode: KeyCodes.enter});
+    }
+
+    public pressEsc() {
+        simulate.keyDown(this.nativeInput, {keyCode: KeyCodes.esc});
+    }
+
+    public typeIn(value: string) {
+        simulateKeyInput(this.nativeInput as HTMLInputElement, value);
+    }
+
+    public blur() {
+        simulate.blur(this.nativeInput);
+    }
+}
