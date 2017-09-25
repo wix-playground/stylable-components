079235f093b7f90996f6462a99df2eea20ab5eda
liorzisman <liorzisman@gmail.com>
2017-09-25 17:38:17 +0200

    renaming of props and driver to make sense

--- test/components/portal.spec.tsx
+++ test/components/portal.spec.tsx
@@ -10,25 +10,25 @@ describe('<Portal />', function() {
     afterEach(function() {clientRenderer.cleanup(); });
 
     it('displays the portal and renders its children', async function() {
-        const {driver: portal} = clientRenderer.render(
+        const {driver} = clientRenderer.render(
             <Portal>
                 <span>Portal Body</span>
             </Portal>).withDriver(PortalTestDriver);
 
         await waitFor(() => {
-            expect(portal.portalRoot).to.be.present();
+            expect(driver.portal).to.be.present();
             expect(portal.content[0]).to.be.present();
         });
     });
 
     it('applies supplied styles to the popup and updates them if changed', async function() {
-        const {container, driver: portal} = clientRenderer.render(
+        const {container, driver} = clientRenderer.render(
             <Portal style={{position: 'absolute'}}>
                 <span>Portal Body</span>
             </Portal>
         ).withDriver(PortalTestDriver);
 
-        await waitFor(() => expect(portal.portalRoot).to.have.nested.property('style.position', 'absolute'));
+        await waitFor(() => expect(driver.portal).to.have.nested.property('style.position', 'absolute'));
 
         clientRenderer.render(
             <Portal style={{position: 'fixed'}}>
@@ -37,18 +37,18 @@ describe('<Portal />', function() {
             container
         );
 
-        await waitFor(() => expect(portal.portalRoot).to.have.nested.property('style.position', 'fixed'));
+        await waitFor(() => expect(driver.portal).to.have.nested.property('style.position', 'fixed'));
     });
 
     it('applies supplied className and id to the popup and updates them if changed', async function() {
-        const {container, driver: portal} = clientRenderer.render(
+        const {container, driver} = clientRenderer.render(
             <Portal className="my-test-class" id="my-test-id">
                 <span>Portal Body</span>
             </Portal>
         ).withDriver(PortalTestDriver);
 
-        await waitFor(() => expect(portal.portalRoot).to.have.nested.property('className', 'my-test-class'));
-        await waitFor(() => expect(portal.portalRoot).to.have.nested.property('id', 'my-test-id'));
+        await waitFor(() => expect(driver.portal).to.have.nested.property('className', 'my-test-class'));
+        await waitFor(() => expect(driver.portal).to.have.nested.property('id', 'my-test-id'));
 
         clientRenderer.render(
             <Portal className="another-test-class" id="another-test-id">
@@ -57,30 +57,30 @@ describe('<Portal />', function() {
             container
         );
 
-        await waitFor(() => expect(portal.portalRoot).to.have.nested.property('className', 'another-test-class'));
-        await waitFor(() => expect(portal.portalRoot).to.have.nested.property('id', 'another-test-id'));
+        await waitFor(() => expect(driver.portal).to.have.nested.property('className', 'another-test-class'));
+        await waitFor(() => expect(driver.portal).to.have.nested.property('id', 'another-test-id'));
     });
 
     it('removes the component when unmounting', async function() {
         const container = document.body.appendChild(document.createElement('div'));
-        const {driver: portal} = clientRenderer.render(
+        const {driver} = clientRenderer.render(
             <Portal>
                 <span>Popup Body</span>
             </Portal>, container).withDriver(PortalTestDriver);
 
-        await waitFor(() => expect(portal.portalRoot).to.be.present());
+        await waitFor(() => expect(driver.portal).to.be.present());
 
         ReactDOM.unmountComponentAtNode(container);
         document.body.removeChild(container);
 
         debugger;
-        await waitFor(() => expect(portal.portalRoot).to.be.absent());
+        await waitFor(() => expect(driver.portal).to.be.absent());
     });
 
     it('updates the portal content if the children are changed', async function() {
         const initialText = 'Portal Body';
         const updatedText = 'Portal Body Updated';
-        const {container, driver: portal} = clientRenderer.render(
+        const {container, driver} = clientRenderer.render(
             <Portal>
                 <span>{initialText}</span>
             </Portal>
@@ -99,7 +99,7 @@ describe('<Portal />', function() {
     });
 
     it('renders the portal in the bottom of the DOM', async function() {
-        const {container, driver: portal} = clientRenderer.render(
+        const {container, driver} = clientRenderer.render(
             <Portal>
                 <span>Portal Body</span>
             </Portal>
@@ -107,21 +107,21 @@ describe('<Portal />', function() {
 
         await waitFor(() => {
             /* tslint:disable:no-bitwise */
-            expect(portal.portalRoot.compareDocumentPosition(portal.content[0]) & Node.DOCUMENT_POSITION_CONTAINED_BY,
+            expect(driver.portal.compareDocumentPosition(portal.content[0]) & Node.DOCUMENT_POSITION_CONTAINED_BY,
                 'children contained in portal').to.equal(Node.DOCUMENT_POSITION_CONTAINED_BY);
-            expect(container.compareDocumentPosition(portal.portalRoot) & Node.DOCUMENT_POSITION_FOLLOWING,
+            expect(container.compareDocumentPosition(driver.portal) & Node.DOCUMENT_POSITION_FOLLOWING,
                 'portal is following the app container').to.equal(Node.DOCUMENT_POSITION_FOLLOWING);
             /* tslint:enable:no-bitwise */
         });
     });
 
     it('renders with a className passed as a prop', async function() {
-        const {driver: portal} = clientRenderer.render(
+        const {driver} = clientRenderer.render(
             <Portal className="test-class">
                 <span>Portal Body</span>
             </Portal>
         ).withDriver(PortalTestDriver);
 
-        await waitFor(() => expect((portal.portalRoot as Element).className).to.contain('test-class'));
+        await waitFor(() => expect((driver.portal as Element).className).to.contain('test-class'));
     });
 });
