37b00e9cb0e3d9b402561917e09c0d1ee3577eab
Lior Zisman <liorzisman@gmail.com>
2017-09-27 20:06:28 +0300

    <Dropdown /> - implemented onOpenStateChange (#339)
    * implemented onOpenStateChange
    
    * validating callback value on match
    
    * the return of the calledOnce
    
    * fully refactored to be a controlled component when it comes to opening and closing
    
    * added calledOnce where it was needed
    
    * PR fixes
    
    * l i n t

--- demo/components/drop-down.demo.tsx
+++ demo/components/drop-down.demo.tsx
@@ -3,19 +3,14 @@ import {ChangeEvent, DropDown} from '../../src';
 
 export interface DropDownDemoState {
     selectedItem: string | undefined;
+    open: boolean;
 }
 
 const items = ['Muffins', 'Pancakes', 'Waffles'];
 
 export class DropDownDemo extends React.Component<{}, DropDownDemoState> {
 
-    public state = {selectedItem: undefined};
-
-    public onItemClick = (e: ChangeEvent<string>) => {
-        this.setState({
-            selectedItem: e.value
-        });
-    }
+    public state = {selectedItem: undefined, open: false};
 
     public render() {
         return (
@@ -26,9 +21,23 @@ export class DropDownDemo extends React.Component<{}, DropDownDemoState> {
                         value={this.state.selectedItem}
                         onChange={this.onItemClick}
                         dataSource={items}
+                        onOpenStateChange={this.onOpenStateChange}
+                        open={this.state.open}
                     />
                 </section>
             </div>
         );
+    }
+
+    private onItemClick = (e: ChangeEvent<string>) => {
+        this.setState({
+            selectedItem: e.value
+        });
+    }
+
+    private onOpenStateChange = (e: ChangeEvent<boolean>) => {
+        this.setState({
+            open: e.value
+        });
     }
 }
--- src/components/drop-down/drop-down.tsx
+++ src/components/drop-down/drop-down.tsx
@@ -24,28 +24,27 @@ export interface DropDownProps extends OptionList, FormInputProps<string>, prope
     children?: React.ReactNode;
     toggleIcon?: React.ComponentType;
     tabIndex?: number;
+    onOpenStateChange?: (e: ChangeEvent<boolean>) => void;
 }
 
 export interface DropDownState {
     dropdown: HTMLDivElement | null;
-    open: boolean;
 }
 
 @stylable(style)
 @properties
 export class DropDown extends React.PureComponent<DropDownProps, DropDownState> {
     public static defaultProps: DropDownProps = {
-        open: false,
         children: [],
         onChange: noop,
         tabIndex: 0,
         toggleIcon: CaretDown,
-        disabled: false
+        disabled: false,
+        onOpenStateChange: noop
     };
 
     public state: DropDownState = {
-        dropdown: null,
-        open: this.props.open!
+        dropdown: null
     };
 
     public onItemClick = (e: ChangeEvent<string>) => {
@@ -71,7 +70,7 @@ export class DropDown extends React.PureComponent<DropDownProps, DropDownState> 
                         <ToggleIcon />
                     </div>
                 </div>
-                <Popup open={this.state.open && !this.props.disabled} anchor={this.state.dropdown}>
+                <Popup open={this.props.open && !this.props.disabled} anchor={this.state.dropdown}>
                     <div className="root">
                         <SelectionList
                             data-automation-id="DROP_DOWN_LIST"
@@ -96,19 +95,19 @@ export class DropDown extends React.PureComponent<DropDownProps, DropDownState> 
 
     private toggleDropdown = () => {
         if (!this.props.disabled) {
-            this.setState({open: !this.state.open});
+            this.props.onOpenStateChange!({value: !this.props.open});
         }
     }
 
     private openDropdown() {
-        if (!this.props.disabled) {
-            this.setState({open: true});
+        if (!this.props.disabled && !this.props.open) {
+            this.props.onOpenStateChange!({value: true});
         }
     }
 
     private closeDropdown() {
-        if (!this.props.disabled) {
-            this.setState({open: false});
+        if (!this.props.disabled && this.props.open) {
+            this.props.onOpenStateChange!({value: false});
         }
     }
 
@@ -120,11 +119,11 @@ export class DropDown extends React.PureComponent<DropDownProps, DropDownState> 
                 break;
             case KeyCodes.ESC:
                 e.preventDefault();
-                this.state.open && this.closeDropdown();
+                this.closeDropdown();
                 break;
             case KeyCodes.DOWN:
                 e.preventDefault();
-                !this.state.open && this.openDropdown();
+                this.openDropdown();
                 break;
         }
     }
