7c6c692b75df6636bd740a3961f25c2ab6ef6b0d
Lior Zisman <liorzi@wix.com>
2017-09-28 14:54:52 +0300

    lint

--- test-kit/components/tree-view-driver.ts
+++ test-kit/components/tree-view-driver.ts
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom';
 import {DriverBase, simulate} from 'test-drive-react';
 import {TreeItem, TreeItemProps, TreeView} from '../../src';
 
