0c313da313af168ae0da318c3cd428cebebfcea9
Evgen Filatov <evgen.filatov@gmail.com>
2017-10-04 15:48:52 +0300

    <Input/> <Button/> components (#448)

---
+++ src/components/input/input.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import {stylable} from 'wix-react-tools';
+import styles from './input.st.css';
+
+export const Input = stylable(styles)(props => {
+    return <input {...props}/>;
+});
