9fc35d60b96f144dcc15337619a3b5734fd23918
Yuri Tkachenko <yuri@ya.ru>
2017-10-04 12:29:55 +0300

    move popup back

--- src/components/auto-complete/auto-complete.tsx
+++ src/components/auto-complete/auto-complete.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {properties, stylable} from 'wix-react-tools';
+import {Popup} from '../../';
 import {ChangeEvent} from '../../types/events';
 import {FormInputProps} from '../../types/forms';
 import {noop} from '../../utils';
@@ -54,16 +55,17 @@ export class AutoComplete extends React.Component<AutoCompleteProps, AutoComplet
                     ref={this.refCallback}
                 />
                 <CaretDown onClick={this.onCaretClick} className="caret" data-automation-id="AUTO_COMPLETE_CARET"/>
-                {
-                    this.props.open && filteredItems!.length > 0 &&
-                    <div className="popup">
-                        <SelectionListView
-                            className="autoCompleteList"
-                            list={list}
-                            onChange={this.onClick}
-                        />
-                    </div>
-                }
+                <Popup
+                    className="root"
+                    anchor={this.state.input}
+                    open={this.props.open && filteredItems!.length > 0}
+                >
+                    <SelectionListView
+                        className="autoCompleteList"
+                        list={list}
+                        onChange={this.onClick}
+                    />
+                </Popup>
             </div>
         );
     }
