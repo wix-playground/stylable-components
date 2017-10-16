12c59c403a8178de83f33389c0a98e894b11c10f
WIXPRESS\danielstr <danielstr@wix.com>
2017-10-16 14:41:00 +0300

    removed maxHeight and onOpenStateChange

--- src/components/popup/popup.tsx
+++ src/components/popup/popup.tsx
@@ -16,7 +16,6 @@ export interface PopupProps extends properties.Props {
     anchorPosition?: PopupPositionPoint;
     popupPosition?: PopupPositionPoint;
     syncWidth?: boolean;
-    maxHeight?: number;
     children?: React.ReactNode;
 }
 
@@ -31,7 +30,6 @@ export class Popup extends React.Component<PopupCompProps> {
         anchorPosition: {vertical: 'bottom', horizontal: 'left'},
         popupPosition: {vertical: 'top', horizontal: 'left'},
         syncWidth: true,
-        maxHeight: 500
     };
 
     private portal: Portal | null;
@@ -57,7 +55,6 @@ export class Popup extends React.Component<PopupCompProps> {
         }
         const newStyle: React.CSSProperties = {position: 'absolute'};
 
-        newStyle.maxHeight = this.props.maxHeight;
         newStyle.transform = '';
         newStyle.WebkitTransform = '';
         if (isPoint(this.props.anchor)) {
