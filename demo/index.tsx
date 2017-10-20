27d2ce6018405f9c2cdbe45f64704a80c8287983
Evgen Filatov <evgen.filatov@gmail.com>
2017-10-20 12:07:32 +0300

    added support of multiple themes for demo

--- demo/index.tsx
+++ demo/index.tsx
@@ -2,42 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {setGlobalConfig, stylable} from 'wix-react-tools';
 import {ComponentsDemo} from './components-demo';
-import wixTheme from './styles/wix.st.css';
+
 import defaultTheme from './styles/default.st.css';
+import wixTheme from './styles/wix.st.css';
+const themes: any = {wix: wixTheme, default: defaultTheme};
 
-const themes = require
-    .context('./styles', true, /\.st.css/)
-    .keys()
-    .reduce((obj, fileName) => {
-        console.log(fileName)
-        const key = fileName.match(/(\w+)\.st\.css/)[1];
-        obj[key] = import('./styles/wix.st.css');
-
-        return obj;
-    }, {})
-
-console.log(themes);
-/*
-const context = require.context('./styles', true, /\.st.css/)
-const themes = context
-    .keys()
-    .reduce((obj, fileName) => {
-        console.log(fileName)
-        const key = fileName.match(/(\w+)\.st\.css/)[1];
-        const path = './styles/' + key + '.st.css';
-        console.log(path);
-        obj[key] = context('./' + key + '.st.css').default;
-
-        return obj;
-    }, {})
-*/
-//const themes: any = {wix: wixTheme, default: defaultTheme};
-const themeName = window.location.search.match(/theme=(\w+)/) && RegExp.$1;
-const theme = themeName && themes[themeName] || themes.default;
+let themeName = window.location.search.match(/theme=(\w+)/) && RegExp.$1;
+themeName = (themeName && themeName in themes) ? themeName : 'default';
 
 setGlobalConfig({devMode: true});
 
-@stylable(theme)
+@stylable(themes[themeName])
 class Demo extends ComponentsDemo {}
 
 const meta = document.createElement('meta');
@@ -47,4 +22,4 @@ document.head.appendChild(meta);
 
 const rootContainer = document.createElement('div');
 document.body.appendChild(rootContainer);
-ReactDOM.render(<Demo />, rootContainer);
+ReactDOM.render(<Demo theme={themeName} themes={Object.keys(themes)}/>, rootContainer);
