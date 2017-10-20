83e92f3479795a415227ab0a1801f192a13bdb48
Evgen Filatov <evgen.filatov@gmail.com>
2017-10-20 18:46:37 +0300

    made as design

--- src/components/tree-view/tree-item-default.tsx
+++ src/components/tree-view/tree-item-default.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {stylable} from 'wix-react-tools';
 import styles from './tree-item.st.css';
 import {TreeItemData, TreeItemProps} from './tree-view';
-import {MinusIcon, PlusIcon} from './tree-view-icons';
 
 const itemIdPrefix = 'TREE_ITEM';
 
@@ -10,12 +9,8 @@ export const TreeItem: React.SFC<TreeItemProps> =
     stylable(styles)(({item, itemRenderer, onItemClick, onIconClick, stateMap}) => {
         const state = stateMap.getItemState(item);
         const itemLabel = item.label.replace(' ', '_');
+        const prefix = `${itemIdPrefix}_${itemLabel}`;
         const TreeNode = itemRenderer;
-        const iconProps = {
-            'data-automation-id': `${itemIdPrefix}_${itemLabel}_ICON`,
-            'onClick': onIconClick && onIconClick.bind(null, item),
-            'aria-hidden': 'true'
-        };
 
         return (
             <li
@@ -23,31 +18,37 @@ export const TreeItem: React.SFC<TreeItemProps> =
                 aria-selected={state!.isSelected ? true : undefined}
                 id={item.label}
                 role="treeitem"
-                data-automation-id={`${itemIdPrefix}_${itemLabel}`}
+                data-automation-id={prefix}
                 className="item"
                 style-state={{selected: state!.isSelected, focused: state!.isFocused, expanded: !!state!.isExpanded}}
                 onClick={onItemClick && onItemClick.bind(null, item)}
             >
                 <div>
-                    {item.children && (state!.isExpanded ?
-                        <MinusIcon {...iconProps} /> : <PlusIcon {...iconProps} />)}
+                    {item.children &&
+                        <div
+                            className={state.isExpanded ? 'close' : 'open'}
+                            data-automation-id={`${prefix}_ICON`}
+                            onClick={onIconClick && onIconClick.bind(null, item)}
+                            aria-hidden
+                        />
+                    }
 
                     <span
-                        data-automation-id={`${itemIdPrefix}_${itemLabel}_LABEL`}
+                        data-automation-id={`${prefix}_LABEL`}
                         className="itemLabel"
                     >
                         {item.label}
                     </span>
                 </div>
-                {item.children && <ul className="nestedTree" role="group">
-                    {state!.isExpanded && item.children.map((child: TreeItemData, index: number) =>
+                {item.children && state.isExpanded && <ul className="nestedTree" role="group">
+                    {item.children.map((child: TreeItemData, index: number) =>
                         <TreeNode
                             item={child}
                             onItemClick={onItemClick}
                             itemRenderer={itemRenderer}
                             onIconClick={onIconClick}
                             stateMap={stateMap}
-                            key={`${index}`}
+                            key={index}
                         />
                     )}
                 </ul>}
