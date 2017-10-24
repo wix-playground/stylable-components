386698ad3533b585cdadda7b16b5d3428d406024
Evgen Filatov <evgen.filatov@gmail.com>
2017-10-24 14:18:45 +0300

    tooltip via portal

--- demo/components/tooltip-demo.tsx
+++ demo/components/tooltip-demo.tsx
@@ -1,15 +1,21 @@
 import * as React from 'react';
-import {Tooltip} from '../../src';
+import {Tooltip, TooltipPosition, Button} from '../../src';
 import {stylable} from 'wix-react-tools';
 
 import styles from './tooltip-demo.st.css';
+
+const positions = ['top', 'left', 'right', 'bottom'];
 
 @stylable(styles)
 export class TooltipDemo extends React.Component {
     render() {
         return <div>
-            <Tooltip/>
-            <Tooltip/>
+            {positions.map((position: any, index) =>
+                <div className='positionButton' key={index}>
+                    <Button data-id={position} children={position}/>
+                    <Tooltip id={position} children={position} position={position}/>
+                </div>
+            )}
         </div>
     }
 }
--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -1,16 +1,57 @@
 import * as React from 'react';
 import {stylable} from 'wix-react-tools';
+
+import {Portal} from '../portal';
 import styles from './tooltip.st.css';
 
+export type Position = 'top' | 'left' | 'right' | 'bottom';
+
 export interface TooltipProps {
+    children?: React.ReactNode;
+	position?: Position;
+	id: string
 }
 export interface TooltipState {
+    //anchor: HTMLElement | null;
 }
 
 
 @stylable(styles)
-export class Tooltip extends React.Component<TooltipProps, TooltipState> {
+class TooltipInner extends React.Component<TooltipProps, TooltipState> {
 	render() {
-		return <div>yo</div>
+		const {children, position} = this.props;
+        return <div
+            className={position}
+        >
+            {children}
+            <svg className="tail" height="5" width="10">
+                <polygon points="0,0 10,0 5,5"/>
+            </svg>
+        </div>
 	}
+}
+
+
+export class Tooltip extends React.Component<TooltipProps> {
+	static defaultProps = {
+		position: 'top'
+	}
+	/*
+    public state = {
+        anchor: null
+    };
+    public componentDidMount() {
+    	const {id} = this.props;
+    	const target = document.querySelector(`[data-id=${id}]`);
+    	this.setState({anchor: target as HTMLElement});
+    }
+    */
+	render() {
+        return (
+            <Portal>
+            	<TooltipInner {...this.props}/>
+            </Portal>
+        );
+	}
+
 }
