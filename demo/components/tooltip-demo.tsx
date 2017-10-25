0edbb97f005209ad898a0249b1abe9931e2eb9ac
Evgen Filatov <evgen.filatov@gmail.com>
2017-10-25 12:26:06 +0300

    working with events

--- demo/components/tooltip-demo.tsx
+++ demo/components/tooltip-demo.tsx
@@ -4,16 +4,43 @@ import {stylable} from 'wix-react-tools';
 
 import styles from './tooltip-demo.st.css';
 
-const positions = ['top', 'left', 'right', 'bottom'];
+const longText = 'Star Wars is an American epic space opera media franchise, centered on a film series created by George Lucas. It depicts the adventures of various characters "a long time ago in a galaxy far, far away". The franchise began in 1977 with the release of the film Star Wars (later subtitled Episode IV: A New Hope in 1981),[2][3] which became a worldwide pop culture phenomenon. It was followed by the successful sequels The Empire Strikes Back (1980) and Return of the Jedi (1983); these three films constitute the original Star Wars trilogy. A prequel trilogy was released between 1999 and 2005, which received mixed reactions from both critics and fans. A sequel trilogy began in 2015 with the release of Star Wars: The Force Awakens. All seven films were nominated for Academy Awards (with wins going to the first two films) and have been commercial successes, with a combined box office revenue of over US$7.5 billion,[4] making Star Wars the third highest-grossing film series.[5] Spin-off films include the animated Star Wars: The Clone Wars (2008) and Rogue One (2016), the latter of which is the first in a planned series of anthology films.'
+
+const samples = [
+    {
+        title: 'Hover to open, leave to close',
+        props: {
+            openTrigger: 'mouseenter',
+            closeTrigger: 'mouseleave',
+            children: 'I am tooltip!'
+        },
+        positions: ['top', 'left', 'right', 'bottom'],
+    },
+    {
+        title: 'Click to open, click to close',
+        props: {
+            openTrigger: 'click',
+            closeTrigger: 'click',
+            children: longText
+        },
+        positions: ['top', 'left', 'right', 'bottom'],
+    }
+]
+
 
 @stylable(styles)
 export class TooltipDemo extends React.Component {
     render() {
         return <div>
-            {positions.map((position: any, index) =>
-                <div className='positionButton' key={index}>
-                    <Button data-id={position} children={position}/>
-                    <Tooltip id={position} children={position} position={position}/>
+            {samples.map((sample, i) =>
+                <div key={i}>
+                    <h4>{sample.title}</h4>
+                    {sample.positions.map((position: any, j) =>
+                        <div className='positionButton' key={j}>
+                            <Button data-id={'id' + i + j} children={position}/>
+                            <Tooltip id={'id' + i + j} position={position} {...sample.props}/>
+                        </div>
+                    )}
                 </div>
             )}
         </div>
--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -10,44 +10,111 @@ export interface TooltipProps {
     children?: React.ReactNode;
     position?: Position;
     id: string;
+    openTrigger?: string;
+    closeTrigger?: string;
 }
 
 export interface TooltipState {
-    style?: React.CSSProperties
+    style?: React.CSSProperties;
+    open: boolean;
 }
 
 @stylable(styles)
 class TooltipInner extends React.Component<TooltipProps, TooltipState> {
     static defaultProps = {
-        position: 'top'
+        position: 'top',
+        openTrigger: 'mouseenter',
+        closeTrigger: 'mouseleave'
     }
     state = {
-        style: undefined
+        style: undefined,
+        open: false
     }
+    private target: HTMLElement | null = null;
+    private events: string[] = [];
+
     public componentDidMount() {
-        const {id} = this.props;
-        const target = document.querySelector(`[data-id=${id}]`);
-        if (!target) {
-                return;
+        this.setTarget();
+        this.bindEvents();
+        this.setStyles();
+    }
+    public componentWillUnmount() {
+        this.unbindEvents();
+    }
+    public componentWillReceiveProps(props: TooltipProps) {
+        this.unbindEvents();
+        this.setTarget();
+        this.bindEvents();
+        this.setStyles();
+    }
+
+    private setTarget() {
+        this.target = document.querySelector(`[data-id=${this.props.id}]`) as HTMLElement;
+    }
+
+    private bindEvents() {
+        if (!this.target) {
+            return;
         }
-        const elemRect = target.getBoundingClientRect();
-        //const bodyRect = document.body.getBoundingClientRect()
+        const {openTrigger, closeTrigger} = this.props;
+        this.events = openTrigger!.split(',')
+            .concat(closeTrigger!.split(','))
+            .filter((val, index, arr) => arr.indexOf(val) === index);
+        this.events.forEach(event => {
+            this.target!.addEventListener(event, this.toggle);
+        });
+    }
+    private unbindEvents() {
+        if (!this.target) {
+            return;
+        }
+        this.events.forEach(event => {
+            this.target!.removeEventListener(event, this.toggle);
+        });
+    }
+    private setStyles() {
+        if (!this.target) {
+            return;
+        }
+        const {position} = this.props;
+        const rect = this.target!.getBoundingClientRect();
+        const top = rect.top + window.scrollY;
+        const left = rect.left + window.scrollX;
         const style = {
-            top: elemRect.top + window.scrollY,
-            left: elemRect.left + window.scrollX,
-            width: elemRect.width,
-            height: elemRect.height
+            top: position === 'bottom' ? (top + rect.height) : top,
+            left: position === 'right' ? (left + rect.width) : left,
+            marginLeft: (position === 'top' || position === 'bottom') ? (rect.width / 2) : undefined,
+            marginTop: (position === 'left' || position === 'right') ? (rect.height / 2) : undefined,
         }
         this.setState({style});
     }
-    render() {
+
+    private toggle = (e: Event) => {
+        const {open} = this.state;
+        const {openTrigger, closeTrigger} = this.props;
+        if (
+            (open && closeTrigger!.indexOf(e.type) !== -1) ||
+            (!open && openTrigger!.indexOf(e.type) !== -1)
+        ) {
+            this.setState({open: !open});
+        }
+    }
+
+    public render() {
         const {children, position} = this.props;
-        const {style} = this.state;
-        console.log(style);
+        const {style, open} = this.state;
+
+        if (!style) {
+            return null;
+        }
 
         return (
-            <div style={style}>
-                <div className={`tooltip ${position}`}>
+            <div
+                className={`root ${position}`}
+                style={style}
+                style-state={{open}}
+            >
+                <div className='tooltip'>
                     {children}
                     <svg className="tail" height="5" width="10">
                         <polygon points="0,0 10,0 5,5"/>
