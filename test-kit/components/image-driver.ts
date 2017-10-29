074c07a6fbf39fe816b4a66f119a7171b427bdaa
Jonathan Marton <jonathan.marton@gmail.com>
2017-10-29 17:42:10 +0200

    Fixing <Image/> Issues (#511)
    This PR addresses:
    1. Adding `errorImage?: string` prop to be used when either `src` or `defaultImage` are broken
    2. Adding `status` state to mark if the image is loading, loaded or erred.  
    3. Adding style states to the stylesheet based on above status.

--- test-kit/components/image-driver.ts
+++ test-kit/components/image-driver.ts
@@ -1,5 +1,7 @@
 import {DriverBase} from 'test-drive-react';
 import {Image} from '../../src';
+import baseStyle from '../../src/components/image/image.st.css';
+import {elementHasStylableState} from '../utils';
 
 export class ImageDriver extends DriverBase {
     public static ComponentClass = Image;
@@ -10,5 +12,9 @@ export class ImageDriver extends DriverBase {
 
     public get source(): string | null {
         return this.nativeElement.getAttribute('src');
+    }
+
+    public hasStylableState(stateName: string): boolean {
+        return elementHasStylableState(this.root, baseStyle, stateName);
     }
 }
