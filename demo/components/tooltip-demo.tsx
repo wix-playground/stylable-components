61600bdc3418b70a49381b36ba3ed2a2ef0b1fc2
Evgen Filatov <evgen.filatov@gmail.com>
2018-04-02 18:07:11 +0300

    added more styles for wix tooltip

--- demo/components/tooltip-demo.tsx
+++ demo/components/tooltip-demo.tsx
@@ -7,19 +7,8 @@ import styles from './tooltip-demo.st.css';
 const longText = `
     Star Wars is an American epic space opera media franchise, centered on a film
     series created by George Lucas. It depicts the adventures of various characters
-    "a long time ago in a galaxy far, far away".  The franchise began in 1977 with
-    the release of the film Star Wars (later subtitled Episode IV: A New Hope in 1981),
-    [2][3] which became a worldwide pop culture phenomenon. It was followed by the successful sequels
-    The Empire Strikes Back (1980) and Return of the Jedi (1983); these three films
-    constitute the original Star Wars trilogy.  A prequel trilogy was released between 1999 and 2005,
-    which received mixed reactions from both critics and fans.  A sequel trilogy began in 2015 with
-    the release of Star Wars: The Force Awakens.  All seven films were nominated for Academy Awards
-    (with wins going to the first two films) and have been commercial successes, with a combined box
-    office revenue of over US$7.5 billion,[4] making Star Wars the third highest-grossing film series.
-    [5] Spin-off films include the animated Star Wars: The Clone Wars (2008) and Rogue One (2016),
-    the latter of which is the first in a planned series of anthology films.
+    "a long time ago in a galaxy far, far away".
 `;
-const longTextContainer = <p style={{width: 400}} children={longText}/>;
 
 const samples = [
     {
@@ -46,7 +35,7 @@ const samples = [
         props: {
             showTrigger: 'click',
             hideTrigger: 'click',
-            children: longTextContainer
+            children: longText
         },
         positions: ['top', 'left', 'right', 'bottom']
     },
@@ -55,7 +44,7 @@ const samples = [
         props: {
             showDelay: 200,
             hideDelay: 300,
-            children: longTextContainer
+            children: longText
         },
         positions: ['top', 'left', 'right', 'bottom']
     },
@@ -64,6 +53,36 @@ const samples = [
         props: {
             children: 'I am tooltip',
             className: 'tooltipWhite'
+        },
+        positions: ['top', 'left', 'right', 'bottom']
+    },
+    {
+        title: 'Small and white',
+        props: {
+            children: 'I am small and white tooltip',
+            className: 'tooltipWhite tooltipSmall',
+            showTrigger: 'click',
+            hideTrigger: 'click'
+        },
+        positions: ['top', 'left', 'right', 'bottom']
+    },
+    {
+        title: 'Small and black',
+        props: {
+            children: 'I am small and black tooltip',
+            className: 'tooltipSmall',
+            showTrigger: 'click',
+            hideTrigger: 'click'
+        },
+        positions: ['top', 'left', 'right', 'bottom']
+    },
+    {
+        title: 'Small and black 2',
+        props: {
+            children: '1',
+            className: 'tooltipSmall',
+            showTrigger: 'click',
+            hideTrigger: 'click'
         },
         positions: ['top', 'left', 'right', 'bottom']
     }
